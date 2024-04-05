MatVeiQaaa/lunaticvibesf

commit 043a957ef6d7a6f3946ad1550211fedb6105dcef

refactor: fix GCC's -Wall -Wno-unknown-pragmas
Mismatched variable types and unused variables, sign-compare - different
signedness comparisons, parentheses - explicit parentheses.

src/db/db_song.cpp
@@ -97,43 +97,40 @@ struct song_all_params
 
     song_all_params(const std::vector<std::any>& queryResult)
     {
-        try 
-        {
-            md5         = ANY_STR(queryResult.at(0));
-            parent      = ANY_STR(queryResult.at(1));
-            file        = ANY_STR(queryResult.at(2));
-            type        = ANY_INT(queryResult.at(3));
-            title       = ANY_STR(queryResult.at(4));
-            title2      = ANY_STR(queryResult.at(5));
-            artist      = ANY_STR(queryResult.at(6));
-            artist2     = ANY_STR(queryResult.at(7));
-            genre       = ANY_STR(queryResult.at(8));
-            version     = ANY_STR(queryResult.at(9));
-            level       = ANY_REAL(queryResult.at(10));
-            bpm         = ANY_REAL(queryResult.at(11));
-            minbpm      = ANY_REAL(queryResult.at(12));
-            maxbpm      = ANY_REAL(queryResult.at(13));
-            length      = ANY_INT(queryResult.at(14));
-            totalnotes  = ANY_INT(queryResult.at(15));
-            stagefile   = ANY_STR(queryResult.at(16));
-            bannerfile  = ANY_STR(queryResult.at(17));
-            gamemode    = ANY_INT(queryResult.at(18));
-            judgerank   = ANY_INT(queryResult.at(19));
-            total       = ANY_INT(queryResult.at(20));
-            playlevel   = ANY_INT(queryResult.at(21));
-            difficulty  = ANY_INT(queryResult.at(22));
-            longnote    = ANY_INT(queryResult.at(23));
-            landmine    = ANY_INT(queryResult.at(24));
-            metricmod   = ANY_INT(queryResult.at(25));
-            stop        = ANY_INT(queryResult.at(26));
-            bga         = ANY_INT(queryResult.at(27));
-            random      = ANY_INT(queryResult.at(28));
-            addtime     = ANY_INT(queryResult.at(29));
-        }
-        catch (std::out_of_range&)
-        {
-        }
-    }   
+        if (queryResult.size() < SONG_PARAM_COUNT)
+            return;
+
+        md5         = ANY_STR(queryResult[0]);
+        parent      = ANY_STR(queryResult[1]);
+        file        = ANY_STR(queryResult[2]);
+        type        = ANY_INT(queryResult[3]);
+        title       = ANY_STR(queryResult[4]);
+        title2      = ANY_STR(queryResult[5]);
+        artist      = ANY_STR(queryResult[6]);
+        artist2     = ANY_STR(queryResult[7]);
+        genre       = ANY_STR(queryResult[8]);
+        version     = ANY_STR(queryResult[9]);
+        level       = ANY_REAL(queryResult[10]);
+        bpm         = ANY_REAL(queryResult[11]);
+        minbpm      = ANY_REAL(queryResult[12]);
+        maxbpm      = ANY_REAL(queryResult[13]);
+        length      = ANY_INT(queryResult[14]);
+        totalnotes  = ANY_INT(queryResult[15]);
+        stagefile   = ANY_STR(queryResult[16]);
+        bannerfile  = ANY_STR(queryResult[17]);
+        gamemode    = ANY_INT(queryResult[18]);
+        judgerank   = ANY_INT(queryResult[19]);
+        total       = ANY_INT(queryResult[20]);
+        playlevel   = ANY_INT(queryResult[21]);
+        difficulty  = ANY_INT(queryResult[22]);
+        longnote    = ANY_INT(queryResult[23]);
+        landmine    = ANY_INT(queryResult[24]);
+        metricmod   = ANY_INT(queryResult[25]);
+        stop        = ANY_INT(queryResult[26]);
+        bga         = ANY_INT(queryResult[27]);
+        random      = ANY_INT(queryResult[28]);
+        addtime     = ANY_INT(queryResult[29]);
+    }
 };
 bool convert_bms(const std::shared_ptr<ChartFormatBMSMeta>& chart, const std::vector<std::any>& in)
 {

src/common/chartformat/chartformat.h
@@ -46,7 +46,7 @@ public:
     HashMD5 fileHash;
     HashMD5 folderHash;
     long long addTime = 0;  // from epoch time
-    int gamemode = 7;       // 5, 7, 9, 10, 14, 24?, 48?
+    unsigned gamemode = 7;  // 5, 7, 9, 10, 14, 24?, 48?
 
     StringContent title;
     StringContent title2;
@@ -55,7 +55,7 @@ public:
     StringContent genre;
     StringContent version;     // mostly known as difficulty name
     int playLevel = 0;
-    int difficulty = 3;            // N/H/A
+    unsigned difficulty = 3; // N/H/A
     double levelEstimated = 0.0;
 
     int totalLength = 0;// in seconds

src/common/chartformat/chartformat_bms.cpp
@@ -122,7 +122,7 @@ int ChartFormatBMS::initWithFile(const Path& filePath, uint64_t randomSeed)
             auto spacePos = std::min(buf.length(), buf.find_first_of(' '));
 
             // supporting single level control flow (#RANDOM, #IF, etc.), matching with LR2's capability
-            if (!randomUsedValues.empty() && randomUsedValues.top().size() < randomValue.top())
+            if (!randomUsedValues.empty() && static_cast<int>(randomUsedValues.top().size()) < randomValue.top())
             {
                 StringContentView v = buf;
                 if (ifValue.empty() && !v.empty() && v[0] == '#' &&
@@ -222,7 +222,7 @@ int ChartFormatBMS::initWithFile(const Path& filePath, uint64_t randomSeed)
                     }
                 }
 
-                if (!randomUsedValues.empty() && randomUsedValues.top().size() == randomValueMax.top())
+                if (!randomUsedValues.empty() && static_cast<int>(randomUsedValues.top().size()) == randomValueMax.top())
                 {
                     randomValueMax.pop();
                     randomValue.pop();
@@ -566,7 +566,7 @@ int ChartFormatBMS::initWithFile(const Path& filePath, uint64_t randomSeed)
             { R"((?i)(hard|hyper))" },
             { R"((?i)(ex|another|insane|lunatic|maniac))" },
         };
-        difficulty = -1;
+        difficulty = 2; // defaults to normal
         for (int i = 4; i >= 1; --i)
         {
             if (RE2::PartialMatch(version, *difficultyRegex[i]))
@@ -585,8 +585,6 @@ int ChartFormatBMS::initWithFile(const Path& filePath, uint64_t randomSeed)
                 break;
             }
         }
-        if (difficulty == -1)
-            difficulty = 2; // defaults to normal
     }
 
     for (size_t i = 0; i <= lastBarIdx; i++)

src/common/difficultytable/table_bms.cpp
@@ -184,8 +184,7 @@ GetResult GET(const std::string& url, std::string& result)
 
 void DifficultyTableBMS::updateFromUrl(std::function<void(DifficultyTable::UpdateResult)> finishedCallback)
 {
-	if (!lunaticvibes::iequals(url.substr(0, 7), "http://") && !lunaticvibes::iequals(url.substr(0, 8), "https://") ||
-		url.substr(8).find('/') == url.npos)
+	if (!lunaticvibes::iequals(url.substr(0, 7), "http://") && !lunaticvibes::iequals(url.substr(0, 8), "https://"))
 	{
 		LOG_ERROR << "[TableBMS] URL error: " << url;
 		finishedCallback(UpdateResult::WEB_PATH_ERROR);

src/game/arena/arena_client.cpp
@@ -445,7 +445,7 @@ void ArenaClient::handlePlayerJoined(const std::shared_ptr<ArenaMessage>& msg)
 	{
 		LOG_WARNING << "[Arena] player ID " << pMsg->playerID << " already exists, removing old";
 
-		for (int i = 0; i < gArenaData.getPlayerCount(); ++i)
+		for (size_t i = 0; i < gArenaData.getPlayerCount(); ++i)
 		{
 			if (gArenaData.playerIDs[i] == pMsg->playerID)
 			{
@@ -478,7 +478,7 @@ void ArenaClient::handlePlayerLeft(const std::shared_ptr<ArenaMessage>& msg)
 	}
 	else
 	{
-		for (int i = 0; i < gArenaData.getPlayerCount(); ++i)
+		for (size_t i = 0; i < gArenaData.getPlayerCount(); ++i)
 		{
 			if (gArenaData.playerIDs[i] == pMsg->playerID)
 			{

src/game/arena/arena_host.cpp
@@ -897,7 +897,7 @@ void ArenaHost::update()
 		for (auto& k : clientsRemoving)
 		{
 			Client& c = clients[k];
-			for (int i = 0; i < gArenaData.getPlayerCount(); ++i)
+			for (size_t i = 0; i < gArenaData.getPlayerCount(); ++i)
 			{
 				if (c.id == gArenaData.getPlayerID(i))
 				{
@@ -981,7 +981,7 @@ void ArenaHost::update()
 	if (!gSelectContext.isArenaReady && !clients.empty() && !hostRequestChartHash.empty())
 	{
 		std::shared_lock l(clientsMutex);
-		int ready = 0;
+		size_t ready = 0;
 		for (auto& [k, cc] : clients)
 		{
 			if (cc.requestChartHash == hostRequestChartHash)
@@ -1021,7 +1021,7 @@ void ArenaHost::update()
 	if (!gArenaData.playing && isLoadingFinished())
 	{
 		std::shared_lock l(clientsMutex);
-		int ready = 0;
+		size_t ready = 0;
 		for (auto& [k, cc] : clients)
 		{
 			if (cc.isLoadingFinished)
@@ -1086,7 +1086,7 @@ void ArenaHost::update()
 	if (gArenaData.playing && isPlayingFinished())
 	{
 		std::shared_lock l(clientsMutex);
-		int finished = 0;
+		size_t finished = 0;
 		for (auto& [k, cc] : clients)
 		{
 			if (cc.isPlayingFinished)
@@ -1111,7 +1111,7 @@ void ArenaHost::update()
 	if (isResultFinished())
 	{
 		std::shared_lock l(clientsMutex);
-		int finished = 0;
+		size_t finished = 0;
 		for (auto& [k, cc] : clients)
 		{
 			if (cc.isResultFinished)

src/game/arena/arena_host.h
@@ -40,7 +40,7 @@ private:
 
 	HashMD5 requestChartPending;
 	std::string requestChartPendingClientKey;		// who has requested this chart
-	int requestChartPendingExistCount = 0;	// if count == clients.size(), inform all clients
+	size_t requestChartPendingExistCount = 0;	// if count == clients.size(), inform all clients
 
 	bool _isLoadingFinished = false; // host status
 	bool _isCreatedRuleset = false;	// host status

src/game/chart/chart_bms.cpp
@@ -267,7 +267,7 @@ void ChartObjectBMS::loadBMS(const ChartFormatBMS& objBms)
 
         // add notes
         {
-            auto push_notes = [&objBms, m, laneCountOneSide](decltype(notes)& notes, eLanePriority priority, LaneCode code, int area)
+            auto push_notes = [&objBms, m](decltype(notes)& notes, eLanePriority priority, LaneCode code, int area)
             {
                 for (unsigned i = 0; i < 10; i++)
                 {
@@ -284,7 +284,7 @@ void ChartObjectBMS::loadBMS(const ChartFormatBMS& objBms)
                     }
                 }
             };
-            auto push_notes_ln = [&objBms, m, laneCountOneSide, &isLnTail](decltype(notes)& notes, LaneCode code, int area)
+            auto push_notes_ln = [&objBms, m, &isLnTail](decltype(notes)& notes, LaneCode code, int area)
             {
                 for (unsigned i = 0; i < 10; i++)
                 {
@@ -437,7 +437,7 @@ void ChartObjectBMS::loadBMS(const ChartFormatBMS& objBms)
 
         // Calculate note times and push to note list
         Segment lastBPMChangedSegment(0, 1);
-        double stopMetre = 0;
+        double stopMetre = 0; // FIXME: set but unused
         Metre barMetre = objBms.metres[m];      // visual metre
 		lunaticvibes::Time beatLength = lunaticvibes::Time::singleBeatLengthFromBPM(bpm);
 
@@ -515,13 +515,13 @@ void ChartObjectBMS::loadBMS(const ChartFormatBMS& objBms)
                     size_t gameLaneIdxMod = gameLaneIdx;
                     size_t laneMin, laneMax;
                     int laneArea;
-                    if (gameLaneIdx == Sc1 || gameLaneIdx >= laneLeftStart && gameLaneIdx <= laneLeftEnd)
+                    if (gameLaneIdx == Sc1 || (gameLaneIdx >= laneLeftStart && gameLaneIdx <= laneLeftEnd))
                     {
                         laneMin = laneLeftStart;
                         laneMax = laneLeftEnd;
                         laneArea = 0;
                     }
-                    else if (gameLaneIdx == Sc2 || gameLaneIdx >= laneRightStart && gameLaneIdx <= laneRightEnd)
+                    else if (gameLaneIdx == Sc2 || (gameLaneIdx >= laneRightStart && gameLaneIdx <= laneRightEnd))
                     {
                         laneMin = laneRightStart;
                         laneMax = laneRightEnd;
@@ -638,7 +638,7 @@ void ChartObjectBMS::loadBMS(const ChartFormatBMS& objBms)
 
                             auto laneIdxScratch = channelToIdx(NoteLaneCategory::Note, laneScratch);
                             if (laneOccupiedByLN[laneScratch] ||
-                                !_noteLists[laneIdxScratch].empty() && notetime - _noteLists[laneIdxScratch].back().time >= threshold_scr_ms)
+                                (!_noteLists[laneIdxScratch].empty() && notetime - _noteLists[laneIdxScratch].back().time >= threshold_scr_ms))
                             {
                                 bool availableLaneFound = false;
                                 for (size_t i = laneMin; i != laneMax + laneStep; i += laneStep)
@@ -676,7 +676,7 @@ void ChartObjectBMS::loadBMS(const ChartFormatBMS& objBms)
                         {
                             auto laneIdxScratch = channelToIdx(NoteLaneCategory::Note, laneScratch);
                             if (laneOccupiedByLN[laneScratch] ||
-                                !_noteLists[laneIdxScratch].empty() && notetime - _noteLists[laneIdxScratch].back().time >= threshold_scr_ms)
+                                (!_noteLists[laneIdxScratch].empty() && notetime - _noteLists[laneIdxScratch].back().time >= threshold_scr_ms))
                             {
                                 std::vector<NoteLaneIndex> placable;
                                 for (NoteLaneIndex i = NoteLaneIndex(laneMin); i != NoteLaneIndex(laneMax + 1); ++ * (size_t*)&i)

commit 3ccdd8df5a69febaf219cb390873392d85ce1be5

fix(db): fix inserting songs
- add support for inserting 'unsigned' to the DB.
- add test for inserting and refreshing songs.
- catch any_cast errors to not crash the game with broken DB, it's not
  enough to force refreshing with F8 though, so song.db needs to be
  deleted.

https://github.com/chown2/lunaticvibesf/issues/6

Fixes: 043a957ef6d7 ("refactor: fix GCC's -Wall -Wno-unknown-pragmas")

src/db/db_conn.cpp
@@ -27,6 +27,7 @@ std::string any_to_str(const std::any& a)
     if (a.type() == typeid(int))              ss << std::any_cast<int>(a);
     else if (a.type() == typeid(bool))        ss << std::any_cast<bool>(a);
     else if (a.type() == typeid(long long))   ss << std::any_cast<long long>(a);
+    else if (a.type() == typeid(unsigned))    ss << std::any_cast<unsigned>(a);
     else if (a.type() == typeid(time_t))      ss << std::any_cast<time_t>(a);
     else if (a.type() == typeid(double))      ss << std::any_cast<double>(a);
     else if (a.type() == typeid(std::string)) ss << "'" << std::any_cast<std::string>(a) << "'";
@@ -35,11 +36,13 @@ std::string any_to_str(const std::any& a)
     return ss.str();
 }
 
+// TODO: use std::variant instead of std::any
 void sql_bind_any(sqlite3_stmt* stmt, int i, const std::any& a)
 {
     if (a.type() == typeid(int)) sqlite3_bind_int(stmt, i, std::any_cast<int>(a));
     else if (a.type() == typeid(bool)) sqlite3_bind_int(stmt, i, int(std::any_cast<bool>(a)));
     else if (a.type() == typeid(long long)) sqlite3_bind_int64(stmt, i, std::any_cast<long long>(a));
+    else if (a.type() == typeid(unsigned)) sqlite3_bind_int64(stmt, i, std::any_cast<unsigned>(a));
     else if (a.type() == typeid(time_t)) sqlite3_bind_int64(stmt, i, std::any_cast<time_t>(a));
     else if (a.type() == typeid(double)) sqlite3_bind_double(stmt, i, std::any_cast<double>(a));
     else if (a.type() == typeid(std::string)) sqlite3_bind_text(stmt, i, std::any_cast<std::string>(a).c_str(), (int)std::any_cast<std::string>(a).length(), SQLITE_TRANSIENT);
@@ -89,9 +92,9 @@ std::vector<std::vector<std::any>> SQLite::query(const std::string_view zsql, st
             case SQLITE_INTEGER: row[i] = sqlite3_column_int64(stmt, i); break;
             case SQLITE_FLOAT: row[i] = sqlite3_column_double(stmt, i); break;
             case SQLITE_TEXT: row[i] = std::make_any<std::string>((const char *)sqlite3_column_text(stmt, i)); break;
-            case SQLITE_BLOB: LOG_ERROR << "[sqlite3] Fetched unsupported type SQLITE_BLOB which is not supported"; break;
-            case SQLITE_NULL: LOG_ERROR << "[sqlite3] Fetched unsupported type SQLITE_NULL which is not supported"; break;
-            default: LOG_ERROR << "[sqlite3] Unknown column type c=" << c; break;
+            case SQLITE_BLOB: LOG_ERROR << "[sqlite3] row[" << i << "]: fetched unsupported type SQLITE_BLOB"; break;
+            case SQLITE_NULL: break; // assume !row[i].has_value()
+            default: LOG_ERROR << "[sqlite3] row[" << i << "]: unknown column type c=" << c; break;
             }
         }
     }

src/db/db_song.cpp
@@ -3,6 +3,7 @@
 #include <regex>
 #include <set>
 #include <thread>
+#include <vector>
 
 #include "common/chartformat/chartformat_types.h"
 #include "common/log.h"
@@ -16,6 +17,7 @@
 #include <boost/asio/post.hpp>
 #include <boost/asio/thread_pool.hpp>
 
+// TODO: NOT NULL everything.
 const char* CREATE_FOLDER_TABLE_STR =
 "CREATE TABLE IF NOT EXISTS folder( "
 "pathmd5 TEXT PRIMARY KEY UNIQUE NOT NULL, "
@@ -26,6 +28,7 @@ const char* CREATE_FOLDER_TABLE_STR =
 "modtime INTEGER"
 ");";
 
+// TODO: NOT NULL everything.
 const char* CREATE_SONG_TABLE_STR =
 "CREATE TABLE IF NOT EXISTS song("
 "md5 TEXT NOT NULL, "           // 0
@@ -382,7 +385,6 @@ bool SongDB::removeChart(const HashMD5& md5, const HashMD5& parent)
     return true;
 }
 
-// search from genre, version, artist, artist2, title, title2
 std::vector<std::shared_ptr<ChartFormatBase>> SongDB::findChartByName(const HashMD5& folder, const std::string& tagRaw, unsigned limit) const
 {
     LOG_INFO << "[SongDB] Search for songs matching: " << tagRaw;
@@ -450,10 +452,9 @@ std::vector<std::shared_ptr<ChartFormatBase>> SongDB::findChartByName(const Hash
     return ret;
 }
 
-// chart may duplicate, return all found
 std::vector<std::shared_ptr<ChartFormatBase>> SongDB::findChartByHash(const HashMD5& target, bool checksum) const
 {
-    // LOG_DEBUG << "[SongDB] Search for song " << target.hexdigest();
+    LOG_DEBUG << "[SongDB] Search for song " << target.hexdigest();
 
     std::vector<std::shared_ptr<ChartFormatBase>> ret;
 
@@ -462,6 +463,7 @@ std::vector<std::shared_ptr<ChartFormatBase>> SongDB::findChartByHash(const Hash
         return ret;
     }
     for (const auto& index : songQueryHashMap.at(target))
+    try
     {
         const auto& r = songQueryPool[index];
         switch (eChartFormat(ANY_INT(r[3])))
@@ -492,6 +494,11 @@ std::vector<std::shared_ptr<ChartFormatBase>> SongDB::findChartByHash(const Hash
         default: break;
         }
     }
+    // TODO: remove after adding NOT NULL to every table field.
+    catch (const std::bad_any_cast& e)
+    {
+        LOG_ERROR << "std::bad_any_cast: " << e.what();
+    }
 
     if (checksum)
     {
@@ -512,7 +519,7 @@ std::vector<std::shared_ptr<ChartFormatBase>> SongDB::findChartByHash(const Hash
         }
     }
 
-    // LOG_DEBUG << "[SongDB] found " << ret.size() << " songs";
+    LOG_DEBUG << "[SongDB] found " << ret.size() << " songs";
     return ret;
 }
 
@@ -1221,6 +1228,7 @@ std::shared_ptr<EntryFolderSong> SongDB::browseSong(const HashMD5& root)
     if (songQueryParentMap.find(root) != songQueryParentMap.end())
     {
         for (const auto& index : songQueryParentMap.at(root))
+        try
         {
             const auto& c = songQueryPool[index];
             auto type = (eChartFormat)ANY_INT(c[3]);
@@ -1249,6 +1257,11 @@ std::shared_ptr<EntryFolderSong> SongDB::browseSong(const HashMD5& root)
             default:
                 break;
             }
+        }
+        // TODO: remove after adding NOT NULL to every table field.
+        catch (const std::bad_any_cast& e)
+        {
+            LOG_ERROR << "std::bad_any_cast: " << e.what();
         }
     }
 

src/db/db_song.h
@@ -56,9 +56,11 @@ protected:
     bool removeChart(const HashMD5& md5, const HashMD5& parent);
     
 public:
-    std::vector<std::shared_ptr<ChartFormatBase>> findChartByName(const HashMD5& folder, const std::string&, unsigned limit = 1000) const;  // search from genre, version, artist, artist2, title, title2
-    std::vector<std::shared_ptr<ChartFormatBase>> findChartByHash(const HashMD5&, bool checksum = true) const;  // chart may duplicate, return a list
-    std::vector<std::shared_ptr<ChartFormatBase>> findChartFromTime(const HashMD5& folder, unsigned long long addTime) const;
+    // Search from genre, version, artist, artist2, title, title2.
+    [[nodiscard]] std::vector<std::shared_ptr<ChartFormatBase>> findChartByName(const HashMD5& folder, const std::string&, unsigned limit = 1000) const;
+    // May contain duplicates.
+    [[nodiscard]] std::vector<std::shared_ptr<ChartFormatBase>> findChartByHash(const HashMD5&, bool checksum = true) const;
+    [[nodiscard]] std::vector<std::shared_ptr<ChartFormatBase>> findChartFromTime(const HashMD5& folder, unsigned long long addTime) const;
 
 protected:
     std::vector<std::vector<std::any>> songQueryPool;

test/CMakeLists.txt
@@ -12,6 +12,7 @@ add_executable(apptest
     common/test_hash.cpp
     common/test_path.cpp
     db/test_score_db.cpp
+    db/test_song_db.cpp
     game/test_graphics.cpp
     game/test_lr2skin.cpp
     game/test_scene_select.cpp

test/db/test_song_db.cpp
@@ -1,0 +1,35 @@
+#include "common/hash.h"
+#include <gmock/gmock.h>
+
+#include <db/db_song.h>
+
+static constexpr auto&& IN_MEMORY_DB_PATH = ":memory:";
+
+TEST(SongDb, SongInserting)
+{
+    SongDB song_db{IN_MEMORY_DB_PATH};
+
+    static const HashMD5 chart_hash{"4257da068c0c860e8556100f07fb94bf"};
+
+    // Insert.
+    {
+        song_db.addSubFolder("bms");
+        song_db.waitLoadingFinish();
+        song_db.prepareCache();
+        const auto charts = song_db.findChartByHash(chart_hash, false);
+        ASSERT_EQ(charts.size(), 1);
+        const auto& chart = charts[0];
+        EXPECT_EQ(chart->fileName, "10k.bms");
+    }
+
+    // Refresh.
+    {
+        song_db.addSubFolder("bms");
+        song_db.waitLoadingFinish();
+        song_db.prepareCache();
+        const auto charts = song_db.findChartByHash(chart_hash, true);
+        ASSERT_EQ(charts.size(), 1);
+        const auto& chart = charts[0];
+        EXPECT_EQ(chart->fileName, "10k.bms");
+    }
+}
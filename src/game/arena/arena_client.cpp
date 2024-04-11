MatVeiQaaa/lunaticvibesf

commit 21819e2b17c6c3d3e31f18c85427d8ef4796dbaa

refactor: refactor Hash struct
- remove {""} constructor sentinel
- make constructor explicit
- make some steps towards using it in constexpr context

src/game/arena/arena_client.cpp
@@ -3,6 +3,7 @@
 #include "arena_internal.h"
 #include "cereal/archives/portable_binary.hpp"
 #include "common/encoding.h"
+#include "common/hash.h"
 #include "common/log.h"
 #include "game/scene/scene_context.h"
 #include "game/runtime/i18n.h"
@@ -498,7 +499,7 @@ void ArenaClient::handleCheckChartExist(const std::shared_ptr<ArenaMessage>& msg
 	ArenaMessageResponse resp(*pMsg);
 
 	ArenaCheckChartExistResp subPayload;
-	subPayload.exist = !g_pSongDB->findChartByHash(pMsg->chartHashMD5String).empty();
+	subPayload.exist = !g_pSongDB->findChartByHash(HashMD5{pMsg->chartHashMD5String}).empty();
 	std::stringstream ss;
 	try
 	{

src/common/difficultytable/table_bms.cpp
@@ -10,6 +10,7 @@
 
 #include "common/encoding.h"
 #include "common/entry/entry_song.h"
+#include "common/hash.h"
 #include "common/log.h"
 #include "common/meta.h"
 
@@ -501,7 +502,7 @@ void DifficultyTableBMS::parseBody(const std::string& content)
 				if (!level.empty() && !md5.empty())
 				{
 					auto pEntry = std::make_shared<EntryChart>();
-					pEntry->md5 = md5;
+					pEntry->md5 = HashMD5{md5};
 					pEntry->_name = name;
 					entries[level].push_back(pEntry);
 				}

src/common/entry/entry_arena.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "common/hash.h"
 #include "common/types.h"
 #include "entry_folder.h"
 
@@ -6,7 +7,7 @@ class EntryFolderArena : public EntryFolderBase
 {
 public:
     EntryFolderArena(const std::string& name, const std::string& name2 = "") :
-        EntryFolderBase(HashMD5(""), name, name2)
+        EntryFolderBase(HashMD5{}, name, name2)
     {
         _type = eEntryType::ARENA_FOLDER;
     }

src/common/entry/entry_course.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include "common/hash.h"
 #include "common/types.h"
 #include "entry.h"
 #include "entry_folder.h"
@@ -11,7 +12,7 @@ class EntryFolderCourse : public EntryFolderBase
 public:
 	EntryFolderCourse() = delete;
 	EntryFolderCourse(StringContentView name = "", StringContentView name2 = "") :
-		EntryFolderBase(HashMD5(""), name, name2)
+		EntryFolderBase(HashMD5{}, name, name2)
 	{
 		_type = eEntryType::COURSE_FOLDER;
 	}

src/common/entry/entry_song.h
@@ -102,7 +102,7 @@ class EntryFolderNewSong : public EntryFolderRegular
 public:
     EntryFolderNewSong() = delete;
     EntryFolderNewSong(StringContentView name, StringContentView name2 = "") :
-        EntryFolderRegular(HashMD5(""), "", name, name2)
+        EntryFolderRegular(HashMD5{}, "", name, name2)
     {
         _type = eEntryType::NEW_SONG_FOLDER;
     }

src/common/entry/entry_table.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "common/hash.h"
 #include "common/types.h"
 #include "entry_folder.h"
 #include "entry_song.h"
@@ -8,7 +9,7 @@ class EntryFolderTable : public EntryFolderRegular
 public:
     EntryFolderTable() = delete;
     EntryFolderTable(StringContentView name, size_t index)
-        : EntryFolderRegular(HashMD5(""), "", name, "")
+        : EntryFolderRegular(HashMD5{}, "", name, "")
         , _index(index)
     {
         _type = eEntryType::CUSTOM_FOLDER;

src/common/hash.h
@@ -15,38 +15,34 @@ private:
     unsigned char data[_Len] = { 0 };
     bool set = false;
 public:
-    Hash() = default;
-    Hash(const std::string& hex)
+    constexpr Hash() = default;
+    explicit Hash(const std::string_view hex)
     {
-        reset();
-        if (!hex.empty())
-        {
-            assert(hex.size() == _Len * 2);
-            set = true;
-            std::string bin = hex2bin(hex);
-            unsigned char* ubin = (unsigned char*)bin.data();
-            std::memcpy(data, ubin, _Len);
-        }
+        assert(hex.size() == _Len * 2);
+        set = true;
+        hex2bin(hex, data);
+    }
+    template<size_t HexLen>
+    explicit Hash(const char(&hex)[HexLen])
+    {
+        static_assert(HexLen - 1 == _Len * 2);
+        set = true;
+        hex2bin(hex, data);
     }
 
     constexpr size_t length() const { return _Len; }
-    bool empty() const { return !set; }
+    // TODO: remove this and use std::optional<Hash> where needed.
+    constexpr bool empty() const { return !set; }
     std::string hexdigest() const { return bin2hex(data, _Len); }
-    const unsigned char* hex() const { return data; }
+    constexpr const unsigned char* hex() const { return data; }
     void reset() { set = false; memset(data, 0, _Len); }
 
-    template <size_t _Len2>
-    bool operator<(const Hash<_Len2>& rhs) const { static_assert(_Len == _Len2); return memcmp(data, rhs.data, _Len) < 0; }
-    template <size_t _Len2>
-    bool operator>(const Hash<_Len2>& rhs) const { static_assert(_Len == _Len2); return memcmp(data, rhs.data, _Len) > 0; }
-    template <size_t _Len2>
-    bool operator<=(const Hash<_Len2>& rhs) const { static_assert(_Len == _Len2); return !(*this > rhs); }
-    template <size_t _Len2>
-    bool operator>=(const Hash<_Len2>& rhs) const { static_assert(_Len == _Len2); return !(*this > rhs); }
-    template <size_t _Len2>
-    bool operator==(const Hash<_Len2>& rhs) const { return _Len == _Len2 && memcmp(data, rhs.data, _Len) == 0; }
-    template <size_t _Len2>
-    bool operator!=(const Hash<_Len2>& rhs) const { return _Len != _Len2 || memcmp(data, rhs.data, _Len) != 0; }
+    bool operator<(const Hash<_Len>& rhs) const { return memcmp(data, rhs.data, _Len) < 0; }
+    bool operator>(const Hash<_Len>& rhs) const { return memcmp(data, rhs.data, _Len) > 0; }
+    bool operator<=(const Hash<_Len>& rhs) const { return !(*this > rhs); }
+    bool operator>=(const Hash<_Len>& rhs) const { return !(*this > rhs); }
+    bool operator==(const Hash<_Len>& rhs) const { return memcmp(data, rhs.data, _Len) == 0; }
+    bool operator!=(const Hash<_Len>& rhs) const { return memcmp(data, rhs.data, _Len) != 0; }
 
     friend struct std::hash<Hash<_Len>>;
 };
@@ -60,8 +56,7 @@ struct std::hash<Hash<_Len>>
     }
 };
 
-typedef Hash<16> HashMD5;
-typedef Hash<32> HashSHA1;
+using HashMD5 = Hash<16>;
 
 HashMD5 md5(std::string_view str);
 HashMD5 md5file(const Path& filePath);

src/common/utils.cpp
@@ -152,19 +152,24 @@ std::string bin2hex(const void* bin, size_t size)
     return res;
 }
 
-std::string hex2bin(std::string_view hex)
+// TODO(C++20): use std::span.
+// LANDMINE.
+void hex2bin(std::string_view hex, unsigned char* buf)
 {
-    std::string res;
-    res.resize(hex.length() / 2 + 1);
     for (size_t i = 0, j = 0; i < hex.length() - 1; i += 2, j++)
     {
-        unsigned char &c = ((unsigned char&)res[j]);
+        unsigned char &c = buf[j];
         unsigned char c1 = tolower(hex[i]);
         unsigned char c2 = tolower(hex[i + 1]);
         c += (c1 + ((c1 >= 'a') ? (10 - 'a') : (-'0'))) << 4;
         c += (c2 + ((c2 >= 'a') ? (10 - 'a') : (-'0')));
     }
-    res[res.length() - 1] = '\0';
+}
+std::string hex2bin(std::string_view hex)
+{
+    std::string res;
+    res.resize(hex.length() / 2);
+    hex2bin(hex, reinterpret_cast<unsigned char*>(res.data()));
     return res;
 }
 
@@ -188,7 +193,7 @@ HashMD5 md5(std::string_view s)
     CryptDestroyHash(hHash);
     CryptReleaseContext(hProv, 0);
 
-    return bin2hex(rgbHash, MD5_LEN);
+    return HashMD5{bin2hex(rgbHash, MD5_LEN)};
 }
 
 HashMD5 md5file(const Path& filePath)
@@ -224,7 +229,7 @@ HashMD5 md5file(const Path& filePath)
     CryptDestroyHash(hHash);
     CryptReleaseContext(hProv, 0);
 
-    return bin2hex(rgbHash, MD5_LEN);
+    return HashMD5{bin2hex(rgbHash, MD5_LEN)};
 }
 
 #else
@@ -240,7 +245,7 @@ static HashMD5 format_hash(const unsigned char* digest, size_t digest_size)
         ret += (high <= 9 ? ('0' + high) : ('A' - 10 + high));
         ret += (low  <= 9 ? ('0' + low)  : ('A' - 10 + low));
     }
-    return ret;
+    return HashMD5{ret};
 }
 
 template <typename DigestUpdater> HashMD5 md5_impl(DigestUpdater updater)

commit 97d38e0d4a03fc7f6bb5103f57ed5af6bd63a13d

fix(game): arena: don't pass empty strings to HashMD5
Fixes https://github.com/chown2/lunaticvibesf/issues/41

Fixes: 21819e2b17c6 ("refactor: refactor Hash struct")

src/game/arena/arena_client.cpp
@@ -541,10 +541,9 @@ void ArenaClient::handleHostRequestChart(const std::shared_ptr<ArenaMessage>& ms
 	auto payload = resp.pack();
 	socket->async_send_to(boost::asio::buffer(*payload), server, std::bind(emptyHandleSend, payload, std::placeholders::_1, std::placeholders::_2));
 
-	auto hash = HashMD5(pMsg->chartHashMD5String);
-
-	if (!hash.empty())
+	if (!pMsg->chartHashMD5String.empty())
 	{
+		HashMD5 hash{pMsg->chartHashMD5String};
 		// select chart
 		gSelectContext.remoteRequestedPlayer = pMsg->requestPlayerName;
 		gSelectContext.remoteRequestedChart = hash;

src/game/arena/arena_host.cpp
@@ -1,6 +1,7 @@
 #include "arena_host.h"
 
 #include "common/encoding.h"
+#include "common/hash.h"
 #include "common/log.h"
 #include "arena_data.h"
 #include "arena_internal.h"
@@ -655,7 +656,9 @@ void ArenaHost::handleRequestChart(const std::string& clientKey, const std::shar
 	Client& c = clients[clientKey];
 	ArenaMessageResponse resp(*pMsg);
 
-	HashMD5 reqChart(pMsg->chartHashMD5String);
+	HashMD5 reqChart;
+	if (!pMsg->chartHashMD5String.empty())
+		reqChart = HashMD5{pMsg->chartHashMD5String};
 	requestChart(reqChart, clientKey);
 
 	auto payload = resp.pack();
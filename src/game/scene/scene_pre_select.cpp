MatVeiQaaa/lunaticvibesf

commit 658e7ba63ee7f18b72137c16cac71d0822b812bb

refactor(game): refactor NEW SONG folder generation

src/game/scene/scene_pre_select.cpp
@@ -146,18 +146,21 @@ void ScenePreSelect::updateLoadSongs()
 
             // NEW SONG
             LOG_INFO << "[List] Generating NEW SONG folder...";
-            auto newSongList = g_pSongDB->findChartFromTime(ROOT_FOLDER_HASH,
-                std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count() - State::get(IndexNumber::NEW_ENTRY_SECONDS));
-            if (!newSongList.empty())
+
+            if (auto newSongList = g_pSongDB->findChartFromTime(
+                    ROOT_FOLDER_HASH, getFileTimeNow() - State::get(IndexNumber::NEW_ENTRY_SECONDS));
+                !newSongList.empty())
             {
                 std::shared_ptr<EntryFolderNewSong> entry = std::make_shared<EntryFolderNewSong>("NEW SONGS");
-                for (auto& c : newSongList)
+                for (auto&& c : newSongList)
                 {
-                    entry->pushEntry(std::make_shared<EntryFolderSong>(c));
+                    entry->pushEntry(std::make_shared<EntryFolderSong>(std::move(c)));
                 }
                 rootFolderProp.dbBrowseEntries.insert(rootFolderProp.dbBrowseEntries.begin(), {entry, nullptr});
+                LOG_INFO << "[List] NEW SONG folder has " << newSongList.size() << " entries";
+            } else {
+                LOG_INFO << "[List] No NEW SONG entries";
             }
-            LOG_INFO << "[List] NEW SONG folder has " << newSongList.size() << " entries";
 
             // ARENA
             LOG_INFO << "[List] Generating ARENA folder...";

commit 66a23c21ec2199c74c1b8ed622c11d01da79d1b8

fix(game): print NEW SONGS size before std::move
Fixes: 658e7ba63ee7 ("refactor(game): refactor NEW SONG folder generation")

src/game/scene/scene_pre_select.cpp
@@ -154,13 +154,13 @@ void ScenePreSelect::updateLoadSongs()
                     ROOT_FOLDER_HASH, getFileTimeNow() - State::get(IndexNumber::NEW_ENTRY_SECONDS));
                 !newSongList.empty())
             {
-                std::shared_ptr<EntryFolderNewSong> entry = std::make_shared<EntryFolderNewSong>("NEW SONGS");
+                LOG_INFO << "[List] Adding " << newSongList.size() << " entries to NEW SONGS";
+                auto entry = std::make_shared<EntryFolderNewSong>("NEW SONGS");
                 for (auto&& c : newSongList)
                 {
                     entry->pushEntry(std::make_shared<EntryFolderSong>(std::move(c)));
                 }
                 rootFolderProp.dbBrowseEntries.insert(rootFolderProp.dbBrowseEntries.begin(), {entry, nullptr});
-                LOG_INFO << "[List] NEW SONG folder has " << newSongList.size() << " entries";
             } else {
                 LOG_INFO << "[List] No NEW SONG entries";
             }
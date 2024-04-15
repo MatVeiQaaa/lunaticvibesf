MatVeiQaaa/lunaticvibesf

commit cefcd079195dcd15bd71b880d2a31a86f864333a

feat(game): add support for joystick buttons with SDL2 input (Linux)

src/game/input/input_mgr_sdl.cpp
@@ -1,17 +1,97 @@
+#include <algorithm>
 #ifndef _WIN32
 #ifdef RENDER_SDL2
+
+#include <array>
+#include <type_traits>
+
+#include <SDL.h>
 
 #include "common/log.h"
 #include "game/graphics/SDL2/input.h"
 
+struct SdlJoystickDeleter
+{
+	void operator()(SDL_Joystick* joystick)
+	{
+		if (joystick != nullptr)
+			SDL_JoystickClose(joystick);
+	}
+};
+using SdlJoystickPtr = std::unique_ptr<SDL_Joystick, SdlJoystickDeleter>;
+
+struct SdlInputHolder
+{
+	std::array<SDL_Joystick*, InputMgr::MAX_JOYSTICK_COUNT> joysticks;
+	SdlInputHolder();
+	SdlInputHolder(const SdlInputHolder&) = delete;
+	SdlInputHolder(SdlInputHolder&&) = delete;
+	SdlInputHolder& operator=(const SdlInputHolder&) = delete;
+	SdlInputHolder& operator=(SdlInputHolder &&) = delete;
+	~SdlInputHolder();
+};
+
+SdlInputHolder::SdlInputHolder()
+{
+	LOG_INFO << "[SDL2] Initializing Joystick";
+	SDL_Init(SDL_INIT_JOYSTICK);
+}
+
+SdlInputHolder::~SdlInputHolder()
+{
+	joysticks.fill(nullptr);
+	LOG_INFO << "[SDL2] De-initializing Joystick";
+	SDL_QuitSubSystem(SDL_INIT_JOYSTICK);
+}
+
+[[nodiscard]] static SdlInputHolder& getInputState()
+{
+	static SdlInputHolder state;
+	return state;
+}
+
 void initInput()
 {
 	LOG_WARNING << "[SDL2] Direct input isn't available, input precision is limited by draw FPS";
+	(void)getInputState();
 }
+
+inline SDL_Joystick* g_game_controller;
 
 void refreshInputDevices()
 {
-	// TODO?
+	auto& state = getInputState();
+	int ret = SDL_NumJoysticks();
+	if (ret < 0)
+	{
+		LOG_ERROR << "[SDL2] SDL_NumJoysticks failed: " << SDL_GetError();
+		return;
+	}
+	if (ret < 1)
+	{
+		LOG_INFO << "[SDL2] No joystick connected";
+		return;
+	}
+	const int joystick_count = ret;
+	LOG_INFO << "[SDL2] Found " << joystick_count
+			 << " joysticks. Please note they are not fully supported with SDL2 backend.";
+	for (int i = 0; i < std::min(joystick_count, static_cast<int>(sdl::state::g_joysticks.size())); ++i)
+	{
+		state.joysticks[i] = SDL_JoystickOpen(i);
+		if (state.joysticks[i] == nullptr)
+		{
+			LOG_ERROR << "[SDL2] Failed to open joystick: " << SDL_GetError();
+			continue;
+		}
+		ret = SDL_JoystickNumButtons(state.joysticks[i]);
+		if (ret < 0)
+		{
+			LOG_ERROR << "[SDL2] SDL_JoystickNumButtons failed: " << SDL_GetError();
+		} else {
+			LOG_INFO << "[SDL2] Joystick " << i << " has " << ret << " buttons, our limit is "
+					 << sdl::state::g_joysticks[i].size();
+		}
+	}
 }
 
 void pollInput()
@@ -28,7 +108,14 @@ bool isKeyPressed(Input::Keyboard key)
 
 bool isButtonPressed(Input::Joystick c, double deadzone)
 {
-	// TODO
+	assert(c.device < sdl::state::g_joysticks.size());
+	const auto& buttons = sdl::state::g_joysticks[c.device];
+	if (c.type == Input::Joystick::Type::BUTTON)
+	{
+		if (buttons.size() < c.index)
+			return false;
+		return buttons[c.index];
+	}
 	return false;
 }
 double getJoystickAxis(size_t device, Input::Joystick::Type type, size_t index)

src/game/graphics/SDL2/input.h
@@ -1,10 +1,14 @@
 #include "common/keymap.h"
 
+#include <array>
+
 #include <SDL.h>
 
-static constexpr size_t SDL_MOUSE_BUTTON_COUNT = 256;
+#include "game/input/input_mgr.h"
 
 namespace sdl::state {
+
+inline constexpr size_t SDL_MOUSE_BUTTON_COUNT = 256;
 
 // `true` for keys currently being pressed down.
 inline bool g_keyboard_scancodes[SDL_NUM_SCANCODES];
@@ -14,6 +18,9 @@ inline int g_mouse_x = 0;
 inline int g_mouse_y = 0;
 // Reset this to `0` after use.
 inline short g_mouse_wheel_delta = 0;
+// `true` for buttons currently being pressed down.
+inline std::array<std::array<bool, InputMgr::MAX_JOYSTICK_BUTTON_COUNT>, InputMgr::MAX_JOYSTICK_COUNT>
+    g_joysticks{};
 
 } // namespace sdl::state
 

src/game/graphics/SDL2/window_SDL2.cpp
@@ -510,6 +510,37 @@ void event_handle()
         case SDL_MOUSEWHEEL:
             sdl::state::g_mouse_wheel_delta = i16_from_i32(e.wheel.y);
             break;
+
+        case SDL_JOYBUTTONDOWN:
+        {
+            assert(e.jbutton.which < static_cast<int>(sdl::state::g_joysticks.size()));
+            auto& buttons = sdl::state::g_joysticks[e.jbutton.which];
+            if (e.jbutton.button < buttons.size())
+            {
+                buttons[e.jbutton.button] = true;
+            }
+            else
+            {
+                LOG_WARNING << "[SDL2] Joystick button is out of range";
+            }
+            break;
+        }
+
+        case SDL_JOYBUTTONUP:
+        {
+            assert(e.jbutton.which < static_cast<int>(sdl::state::g_joysticks.size()));
+            auto& buttons = sdl::state::g_joysticks[e.jbutton.which];
+            if (e.jbutton.button < buttons.size())
+            {
+                buttons[e.jbutton.button] = false;
+            }
+            else
+            {
+                LOG_WARNING << "[SDL2] Joystick button is out of range";
+            }
+            break;
+        }
+
         default:
             break;
         }

commit 23b8f140ba9838c99c360df5a657d62c19bc5308

refactor(game): tidy-up input_mgr_sdl.cpp includes
Fixes: cefcd079195d ("feat(game): add support for joystick buttons with SDL2 input (Linux)")

src/game/input/input_mgr_sdl.cpp
@@ -1,9 +1,8 @@
-#include <algorithm>
 #ifndef _WIN32
 #ifdef RENDER_SDL2
 
+#include <algorithm>
 #include <array>
-#include <type_traits>
 
 #include <SDL.h>
 
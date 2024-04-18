MatVeiQaaa/lunaticvibesf

commit 0657c0177b9fa158aeed10648d5534ac6272e511

refactor(common): make Time methods constexpr
Also, simplify some operator implementations and add some [[nodiscard]].

src/common/beat.h
@@ -43,13 +43,14 @@ private:
 	decltype(std::declval<timeNormRes>().count()) _regular;
 	decltype(std::declval<timeHighRes>().count()) _highres;
 public:
+	// TODO: have a separate helper that would construct with current time.
 	Time()
 	{
 		auto now = std::chrono::system_clock::now().time_since_epoch();
 		_regular = std::chrono::duration_cast<timeNormRes>(now).count();
 		_highres = std::chrono::duration_cast<timeHighRes>(now).count();
 	}
-	Time(long long n, bool init_with_high_resolution_timestamp = false)
+	constexpr Time(long long n, bool init_with_high_resolution_timestamp = false) : _regular(), _highres()
 	{
 		if (init_with_high_resolution_timestamp || n > LLONG_MAX / 1000000)
 		{
@@ -62,31 +63,29 @@ public:
 			_highres = std::chrono::duration_cast<timeHighRes>(timeNormRes(n)).count();
 		}
 	}
-	Time(const Time& t) :_regular(t._regular), _highres(t._highres) {}
-	~Time() {}
 
-	static Time singleBeatLengthFromBPM(BPM bpm)
+	[[nodiscard]] static constexpr Time singleBeatLengthFromBPM(BPM bpm)
 	{
 		using namespace std::chrono;
 		return Time(6e4 / bpm * duration_cast<timeHighRes>(1ms).count(), true);
 	}
 
-	Time operator-  () const { return Time(-_highres, true); }
-	Time operator+  (const Time& rhs) const { Time tmp(*this); tmp._highres += rhs._highres; tmp._regular = tmp._highres / 1000000; return tmp; }
-	Time operator-  (const Time& rhs) const { Time tmp(*this); tmp._highres -= rhs._highres; tmp._regular = tmp._highres / 1000000; return tmp; }
-	Time operator*  (const double rhs) const { Time tmp(*this); tmp._highres *= rhs; tmp._regular = tmp._highres / 1000000; return tmp; }
-	Time& operator+= (const Time& rhs) { _highres += rhs._highres; _regular = _highres / 1000000; return *this; }
-	Time& operator-= (const Time& rhs) { _highres -= rhs._highres; _regular = _highres / 1000000; return *this; }
-	bool   operator<  (const Time& rhs) const { return _highres < rhs._highres; }
-	bool   operator>  (const Time& rhs) const { return _highres > rhs._highres; }
-	bool   operator<= (const Time& rhs) const { return _highres <= rhs._highres; }
-	bool   operator>= (const Time& rhs) const { return _highres >= rhs._highres; }
-	bool   operator== (const Time& rhs) const { return _highres == rhs._highres; }
-	bool   operator!= (const Time& rhs) const { return _highres != rhs._highres; }
+	constexpr Time operator- () const { return Time(-_highres, true); }
+	constexpr Time operator+ (const Time& rhs) const { return {_highres + rhs._highres, true}; }
+	constexpr Time operator- (const Time& rhs) const { return {_highres - rhs._highres, true}; }
+	constexpr Time operator* (const double rhs) const { return {static_cast<long>(_highres * rhs), true}; }
+	constexpr Time& operator+= (const Time& rhs) { _highres += rhs._highres; _regular = _highres / 1000000; return *this; }
+	constexpr Time& operator-= (const Time& rhs) { _highres -= rhs._highres; _regular = _highres / 1000000; return *this; }
+	constexpr bool operator< (const Time& rhs) const { return _highres < rhs._highres; }
+	constexpr bool operator> (const Time& rhs) const { return _highres > rhs._highres; }
+	constexpr bool operator<= (const Time& rhs) const { return _highres <= rhs._highres; }
+	constexpr bool operator>= (const Time& rhs) const { return _highres >= rhs._highres; }
+	constexpr bool operator== (const Time& rhs) const { return _highres == rhs._highres; }
+	constexpr bool operator!= (const Time& rhs) const { return _highres != rhs._highres; }
 	friend inline std::ostream& operator<< (std::ostream& os, const Time& t) { return os << t._regular << "ms / " << t._highres << "ns"; }
 
-	constexpr decltype(_regular) norm() const { return _regular; }  // ms
-	constexpr decltype(_highres) hres() const { return _highres; }  // ns
+	[[nodiscard]] constexpr auto norm() const { return _regular; }  // ms
+	[[nodiscard]] constexpr auto hres() const { return _highres; }  // ns
 };
 #pragma warning(pop)
 

src/game/scene/scene_play.cpp
@@ -2788,7 +2788,7 @@ void ScenePlay::procCommonNotes()
 
 void ScenePlay::changeKeySampleMapping(const lunaticvibes::Time& t)
 {
-    static const lunaticvibes::Time MIN_REMAP_INTERVAL{ 1000 };
+    static constexpr lunaticvibes::Time MIN_REMAP_INTERVAL{ 1000 };
 
     auto changeKeySample = [&](Input::Pad k, int slot)
     {

test/game/test_graphics.cpp
@@ -9,7 +9,7 @@ public:
 };
 
 static Rect TEST_RECT{ 0, 0, 256, 256 };
-static lunaticvibes::Time t0{ 1 }, t1{ 2 }, t2{ 3 }, t3{ 4 }, t4{ 5 }, t5{ 6 }, t6{ 7 }, t7{ 8 };
+static constexpr lunaticvibes::Time t0{1}, t1{2}, t2{3}, t3{4}, t4{5}, t5{6}, t6{7}, t7{8};
 class mock_Texture : public Texture
 {
 public:

commit c15110bb510dc6037f335858f28789351b4a480c

fix(common): operate on Time using correct underlying type
'long' is 32-bit on Windows. Insane.

Fixes https://github.com/chown2/lunaticvibesf/issues/12 (LN
Chaos/gimmick bug).

Fixes https://github.com/chown2/lunaticvibesf/issues/23 (Tempo glitch).

Fixes: 0657c0177b9f ("refactor(common): make Time methods constexpr")

src/common/beat.h
@@ -15,7 +15,7 @@ protected:
 public:
 	Metre() : fraction() {}
 	Metre(long long division_level, long long multiple_level) : fraction(division_level, multiple_level, false) {}
-	Metre(double value) : fraction(static_cast<long long>(value * 1e12), 1e12, true) {}
+	Metre(double value) : fraction(static_cast<long long>(value * 1e12), 1'000'000'000'000, true) {}
 	~Metre() override {}
 
 	double toDouble() const { return operator double(); }
@@ -64,13 +64,13 @@ public:
 	[[nodiscard]] static constexpr Time singleBeatLengthFromBPM(BPM bpm)
 	{
 		using namespace std::chrono;
-		return Time(6e4 / bpm * duration_cast<timeHighRes>(1ms).count(), true);
+		return {static_cast<long long>(6e4 / bpm * duration_cast<timeHighRes>(1ms).count()), true};
 	}
 
-	constexpr Time operator- () const { return Time(-_highres, true); }
+	constexpr Time operator- () const { return {-_highres, true}; }
 	constexpr Time operator+ (const Time& rhs) const { return {_highres + rhs._highres, true}; }
 	constexpr Time operator- (const Time& rhs) const { return {_highres - rhs._highres, true}; }
-	constexpr Time operator* (const double rhs) const { return {static_cast<long>(_highres * rhs), true}; }
+	constexpr Time operator* (const double rhs) const { return {static_cast<long long>(_highres * rhs), true}; }
 	constexpr Time& operator+= (const Time& rhs) { _highres += rhs._highres; _regular = _highres / 1000000; return *this; }
 	constexpr Time& operator-= (const Time& rhs) { _highres -= rhs._highres; _regular = _highres / 1000000; return *this; }
 	constexpr bool operator< (const Time& rhs) const { return _highres < rhs._highres; }

test/CMakeLists.txt
@@ -6,6 +6,7 @@ include(GoogleTest)
 add_executable(apptest
     test_main.cpp
     test_config.cpp
+    common/test_beat.cpp
     common/test_encoding.cpp
     common/test_fraction.cpp
     common/test_chartformat_bms.cpp

test/common/test_beat.cpp
@@ -1,0 +1,28 @@
+#include <common/beat.h>
+
+#include <cstdint>
+#include <limits>
+
+#include <gmock/gmock.h>
+
+using uint32_t = std::uint32_t;
+
+TEST(Beat, TimeOperators)
+{
+    using lunaticvibes::Time;
+    EXPECT_EQ(-Time{1}, Time{-1});
+    EXPECT_EQ((Time{1} + Time{1}), Time{2});
+    EXPECT_EQ((Time{2} - Time{1}), Time{1});
+    EXPECT_EQ((Time{std::numeric_limits<uint32_t>().max(), true} * 4.0),
+              (Time{static_cast<long long>(static_cast<double>(std::numeric_limits<uint32_t>().max()) * 4.0), true}));
+    EXPECT_EQ((Time{1} += Time{1}), Time{2});
+    EXPECT_EQ((Time{2} -= Time{1}), Time{1});
+    EXPECT_LT(Time{0}, Time{1});
+    EXPECT_GT(Time{1}, Time{0});
+    EXPECT_LE(Time{0}, Time{0});
+    EXPECT_LE(Time{0}, Time{1});
+    EXPECT_GE(Time{1}, Time{0});
+    EXPECT_GE(Time{1}, Time{1});
+    EXPECT_EQ(Time{0}, Time{0});
+    EXPECT_NE(Time{0}, Time{1});
+}
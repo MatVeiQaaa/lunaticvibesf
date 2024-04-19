MatVeiQaaa/lunaticvibesf

commit a5fcb2b3c31fb6a93b9d4ca3f8bb1cdc075af6b4

perf(game): only get motionStartTimer value once
Up to 1.5 times faster SpriteBase::update.

src/game/graphics/sprite.cpp
@@ -81,17 +81,14 @@ bool SpriteBase::updateMotion(const lunaticvibes::Time& rawTime)
     lunaticvibes::Time time;
 
     // Check if timer is valid
-    if (State::get(motionStartTimer) < 0 || State::get(motionStartTimer) == TIMER_NEVER)
+    const auto motionStartTime = State::get(motionStartTimer);
+    if (motionStartTime < 0 || motionStartTime == TIMER_NEVER)
         return false;
 
 	// Check if timer is 140
-    if (motionStartTimer == IndexTimer::MUSIC_BEAT)
+    if (motionStartTimer != IndexTimer::MUSIC_BEAT)
     {
-        time = State::get(IndexTimer::MUSIC_BEAT);
-    }
-    else
-    {
-        time = rawTime - lunaticvibes::Time(State::get(motionStartTimer), false);
+        time = rawTime - lunaticvibes::Time(motionStartTime, false);
     }
 
     // Check if the sprite is not visible yet

commit d23682c3fec0743ffe1c378fc2e5373060cba823

fix(game): use IndexTimer::MUSIC_BEAT value
Fixes https://github.com/chown2/lunaticvibesf/issues/70 ([Skin] Timer
140 beat no working).

Fixes: a5fcb2b3c31f ("perf(game): only get motionStartTimer value once")

src/game/graphics/sprite.cpp
@@ -78,17 +78,15 @@ bool SpriteBase::updateMotion(const lunaticvibes::Time& rawTime)
     if (frameCount < 1)
         return false;
 
-    lunaticvibes::Time time;
-
     // Check if timer is valid
-    const auto motionStartTime = State::get(motionStartTimer);
-    if (motionStartTime < 0 || motionStartTime == TIMER_NEVER)
+    lunaticvibes::Time time = State::get(motionStartTimer);
+    if (time < 0 || time == TIMER_NEVER)
         return false;
 
 	// Check if timer is 140
     if (motionStartTimer != IndexTimer::MUSIC_BEAT)
     {
-        time = rawTime - lunaticvibes::Time(motionStartTime, false);
+        time = rawTime - time;
     }
 
     // Check if the sprite is not visible yet
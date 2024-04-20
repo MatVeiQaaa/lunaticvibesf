MatVeiQaaa/lunaticvibesf

commit d100cff23a096634790c2b63de6e5b748c04fb58

perf(game): put Image creating and more of BGA loading to other threads
SDL_Image is thread-safe.

So far looks like the game no longer drops to 10 FPS when loading
picture-based BGAs.

src/game/scene/scene_play.cpp
@@ -1233,33 +1233,28 @@ void ScenePlay::loadChart()
                     return;
                 }
 
-                std::list<std::pair<size_t, Path>> mapBgaFiles;
-                auto loadBgaFiles = [&]() {
-                    if (shouldDiscard(*this)) return;
-                    for (auto& [i, pBmp] : mapBgaFiles)
-                    {
-                        if (pBmp.is_absolute())
-                            gPlayContext.bgaTexture->addBmp(i, pBmp);
-                        else
-                            gPlayContext.bgaTexture->addBmp(i, chartDir / pBmp);
-                        ++bmpLoaded;
-                    }
-                };
+                const auto thread_count = std::thread::hardware_concurrency();
+                boost::asio::thread_pool pool(thread_count > 2 ? thread_count : 1);
                 for (size_t i = 0; i < _pChart->bgaFiles.size(); ++i)
                 {
                     if (shouldDiscard(*this)) return;
                     const auto& bmp = _pChart->bgaFiles[i];
                     if (bmp.empty()) continue;
-
-                    mapBgaFiles.emplace_back(i, PathFromUTF8(bmp));
-
-                    // load 8 bmps each frame
-                    if (mapBgaFiles.size() >= 8)
-                    {
-                        pushAndWaitMainThreadTask<void>(loadBgaFiles);
-                        mapBgaFiles.clear();
-                    }
+                    boost::asio::post(pool, [&bmp, this, i, &chartDir]() {
+                        if (shouldDiscard(*this)) return;
+                        const auto pBmp = PathFromUTF8(bmp);
+                        if (pBmp.is_absolute())
+                        {
+                            LOG_WARNING << "[Play] Absolute path to BGA picture, this is forbidden";
+                        }
+                        else
+                        {
+                            gPlayContext.bgaTexture->addBmp(i, chartDir / pBmp);
+                        }
+                        ++bmpLoaded;
+                    });
                 }
+                pool.wait();
 
                 if (shouldDiscard(*this))
                 {
@@ -1268,8 +1263,6 @@ void ScenePlay::loadChart()
                 }
 
                 LOG_DEBUG << "[Play] BGA loaded";
-                loadBgaFiles();
-                mapBgaFiles.clear();
                 if (bmpLoaded > 0)
                 {
                     gPlayContext.bgaTexture->setLoaded();

src/game/graphics/SDL2/common_SDL2.cpp
@@ -154,27 +154,19 @@ Image::Image(const char* path, std::shared_ptr<SDL_RWops>&& rw): _path(path), _p
     const std::string_view pathView{path};
     if (isTGA(pathView))
     {
-        _pSurface = std::shared_ptr<SDL_Surface>(
-            pushAndWaitMainThreadTask<SDL_Surface*>(std::bind(IMG_LoadTGA_RW, &*_pRWop)),
-            std::bind(pushAndWaitMainThreadTask<void, SDL_Surface*>, SDL_FreeSurface, _1));
+        _pSurface = std::shared_ptr<SDL_Surface>(IMG_LoadTGA_RW(_pRWop.get()), SDL_FreeSurface);
     }
     else if (isPNG(pathView))
     {
-        _pSurface = std::shared_ptr<SDL_Surface>(
-            pushAndWaitMainThreadTask<SDL_Surface*>(std::bind(IMG_LoadPNG_RW, &*_pRWop)),
-            std::bind(pushAndWaitMainThreadTask<void, SDL_Surface*>, SDL_FreeSurface, _1));
+        _pSurface = std::shared_ptr<SDL_Surface>(IMG_LoadPNG_RW(_pRWop.get()), SDL_FreeSurface);
     }
     else if (isGIF(pathView))
     {
-        _pSurface = std::shared_ptr<SDL_Surface>(
-            pushAndWaitMainThreadTask<SDL_Surface*>(std::bind(IMG_LoadGIF_RW, &*_pRWop)),
-            std::bind(pushAndWaitMainThreadTask<void, SDL_Surface*>, SDL_FreeSurface, _1));
+        _pSurface = std::shared_ptr<SDL_Surface>(IMG_LoadGIF_RW(_pRWop.get()), SDL_FreeSurface);
     }
     else
     {
-        _pSurface = std::shared_ptr<SDL_Surface>(
-            pushAndWaitMainThreadTask<SDL_Surface*>(std::bind(IMG_Load_RW, &*_pRWop, SDL_LOAD_NOAUTOFREE)),
-            std::bind(pushAndWaitMainThreadTask<void, SDL_Surface*>, SDL_FreeSurface, _1));
+        _pSurface = std::shared_ptr<SDL_Surface>(IMG_Load_RW(_pRWop.get(), SDL_LOAD_NOAUTOFREE), SDL_FreeSurface);
     }
 
     if (!_pSurface)
@@ -193,8 +185,7 @@ void Image::setTransparentColorRGB(Color c)
     if (_pSurface)
     {
         auto pSurfaceTmp = std::shared_ptr<SDL_Surface>(
-            pushAndWaitMainThreadTask<SDL_Surface*>(std::bind(SDL_CreateRGBSurfaceWithFormat, 0, _pSurface->w, _pSurface->h, 32, SDL_PIXELFORMAT_RGBA32)),
-            std::bind(pushAndWaitMainThreadTask<void, SDL_Surface*>, SDL_FreeSurface, _1));
+            SDL_CreateRGBSurfaceWithFormat(0, _pSurface->w, _pSurface->h, 32, SDL_PIXELFORMAT_RGBA32), SDL_FreeSurface);
         SDL_SetColorKey(&*_pSurface, SDL_TRUE, SDL_MapRGB(_pSurface->format, c.r, c.g, c.b));
         SDL_Rect rc = _pSurface->clip_rect;
         SDL_BlitSurface(&*_pSurface, &rc, &*pSurfaceTmp, &rc);
@@ -453,17 +444,14 @@ void Texture::draw(const Rect& srcRect, RectF dstRect,
 
 TextureFull::TextureFull(const Color& c): Texture(nullptr)
 {
-    pushAndWaitMainThreadTask<void>([this, &c]()
-        {
-            auto surface = SDL_CreateRGBSurfaceWithFormat(0, 1, 1, 24, SDL_PIXELFORMAT_RGB24);
-            textureRect = { 0,0,1,1 };
-            SDL_FillRect(&*surface, &textureRect, SDL_MapRGBA(surface->format, c.r, c.g, c.b, c.a));
-            _pTexture = std::shared_ptr<SDL_Texture>(
-                pushAndWaitMainThreadTask<SDL_Texture*>(std::bind(SDL_CreateTextureFromSurface, gFrameRenderer, surface)),
-                std::bind(pushAndWaitMainThreadTask<void, SDL_Texture*>, SDL_DestroyTexture, _1));
-            SDL_FreeSurface(surface);
-        });
+    auto surface = SDL_CreateRGBSurfaceWithFormat(0, 1, 1, 24, SDL_PIXELFORMAT_RGB24);
+    textureRect = {0, 0, 1, 1};
+    SDL_FillRect(&*surface, &textureRect, SDL_MapRGBA(surface->format, c.r, c.g, c.b, c.a));
+    _pTexture = std::shared_ptr<SDL_Texture>(
+        pushAndWaitMainThreadTask<SDL_Texture *>(std::bind(SDL_CreateTextureFromSurface, gFrameRenderer, surface)),
+        std::bind(pushAndWaitMainThreadTask<void, SDL_Texture *>, SDL_DestroyTexture, _1));
     loaded = true;
+    SDL_FreeSurface(surface);
 }
 
 TextureFull::TextureFull(const Image& srcImage) : Texture(srcImage) {}

src/game/graphics/texture_extra.cpp
@@ -1,6 +1,7 @@
 
 #include <algorithm>
 
+#include "common/sysutil.h"
 #include "game/chart/chart_bms.h"
 #include "texture_extra.h"
 #include "game/scene/scene_context.h"
@@ -186,7 +187,6 @@ bool TextureBmsBga::addBmp(size_t idx, Path pBmp)
 			Image layerImg(pBmp);
 			layerImg.setTransparentColorRGB(Color(0, 0, 0, 255));
 			objs_layer[idx].pt = std::make_shared<Texture>(layerImg);
-
 			LOG_DEBUG << "[TextureBmsBga] added pic: " << pBmp;
 			return true;
 		}
@@ -194,9 +194,7 @@ bool TextureBmsBga::addBmp(size_t idx, Path pBmp)
 	else
 	{
 		objs[idx].type = obj::Ty::EMPTY;
-
 		objs_layer[idx].type = obj::Ty::EMPTY;
-
 		LOG_DEBUG << "[TextureBmsBga] file not found, added dummy: " << pBmp;
 	}
 	return false;

commit bc85d73fc4ccf068e999c86a085d812e2e6a8b1b

fix(game): don't use a thread pool for TextureBmsBga
This is still miles faster than it was before, as the heaviest part, the
image loading itself, is now not done on the main thread.

Fixes https://github.com/chown2/lunaticvibesf/issues/76 (Data races in
TextureBmsBga).

Fixes: d100cff23a09 ("perf(game): put Image creating and more of BGA loading to other threads")

src/game/scene/scene_play.cpp
@@ -1237,14 +1237,16 @@ void ScenePlay::loadChart()
                     return;
                 }
 
-                const auto thread_count = std::thread::hardware_concurrency();
-                boost::asio::thread_pool pool(thread_count > 2 ? thread_count : 1);
+                // TODO: make addBmp thread-safe and use the thread pool here.
+                // const auto thread_count = std::thread::hardware_concurrency();
+                // boost::asio::thread_pool pool(thread_count > 2 ? thread_count : 1);
                 for (size_t i = 0; i < _pChart->bgaFiles.size(); ++i)
                 {
                     if (shouldDiscard(*this)) return;
                     const auto& bmp = _pChart->bgaFiles[i];
                     if (bmp.empty()) continue;
-                    boost::asio::post(pool, [&bmp, this, i, &chartDir]() {
+                    // boost::asio::post(pool, [&bmp, this, i, &chartDir]() {
+                     {
                         if (shouldDiscard(*this)) return;
                         const auto pBmp = PathFromUTF8(bmp);
                         if (pBmp.is_absolute())
@@ -1260,9 +1262,10 @@ void ScenePlay::loadChart()
                             gPlayContext.bgaTexture->addBmp(i, p);
                         }
                         ++bmpLoaded;
-                    });
+                    }
+                    // });
                 }
-                pool.wait();
+                // pool.wait();
 
                 if (shouldDiscard(*this))
                 {
bfae76689f231df264e76f7c1eab6d3c3cba4737
chown2 <kuso@cock.email>
2024-04-22 21:28:13 +0000

    refactor(common): add IWYU export for entry_types.h

--- src/common/entry/entry_types.h
+++ src/common/entry/entry_types.h
@@ -1,6 +1,6 @@
 #pragma once
-#include "entry_folder.h"
-#include "entry_song.h"
-#include "entry_table.h"
-#include "entry_course.h"
-#include "entry_arena.h"
+#include "entry_folder.h" // IWYU pragma: export
+#include "entry_song.h" // IWYU pragma: export
+#include "entry_table.h" // IWYU pragma: export
+#include "entry_course.h" // IWYU pragma: export
+#include "entry_arena.h" // IWYU pragma: export

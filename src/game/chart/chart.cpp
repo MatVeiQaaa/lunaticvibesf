MatVeiQaaa/lunaticvibesf

commit ebb5cd061f64517fc29c38592c51d06282ea455f

refactor(game): don't depend on global state in rulesets
Also add shiftFiveKeyForSevenKeyIndex().

Only `gChartContext.started && gPlayContext.replayNew` left, which are
not an issue for now.
Prerequisite for importing replays.

Playing, replays, mybest and auto tested. Arena not tested.

src/game/chart/chart.cpp
@@ -116,14 +116,7 @@ chart::NoteLaneIndex chart::KeyToLane(int keys, Input::Pad pad)
                 Sc2, Sc2, _, _, N21, N22, N23, N24, N25, _, _, _, _, _, _,
             },
         };
-        if (gPlayContext.shift1PNotes5KFor7KSkin)
-        {
-            return gPlayContext.shift2PNotes5KFor7KSkin ? lane[3][pad] : lane[2][pad];
-        }
-        else
-        {
-            return gPlayContext.shift2PNotes5KFor7KSkin ? lane[1][pad] : lane[0][pad];
-        }
+        return lane[gPlayContext.shiftFiveKeyForSevenKeyIndex(true)][pad];
     }
     case 7:
     case 14:

src/game/ruleset/ruleset_bms.cpp
@@ -52,8 +52,9 @@ void setJudgeInternalTimer2P(RulesetBMS::JudgeType judge, long long t)
 }
 
 RulesetBMS::RulesetBMS(std::shared_ptr<ChartFormatBase> format, std::shared_ptr<ChartObjectBase> chart,
-    PlayModifierGaugeType gauge, GameModeKeys keys, RulesetBMS::JudgeDifficulty difficulty, double health, RulesetBMS::PlaySide side) :
-    RulesetBase(std::move(format), std::move(chart)), _judgeDifficulty(difficulty)
+                       PlayModifiers mods, GameModeKeys keys, RulesetBMS::JudgeDifficulty difficulty, double health,
+                       RulesetBMS::PlaySide side, const int fiveKeyMapIndex)
+    : RulesetBase(std::move(format), std::move(chart)), _judgeDifficulty(difficulty)
 {
 
     static const NoteLaneTimerMap bombTimer5k[] = {
@@ -239,23 +240,20 @@ RulesetBMS::RulesetBMS(std::shared_ptr<ChartFormatBase> format, std::shared_ptr<
     switch (keys)
     {
     case 5:
-    case 10: 
-    {
-        size_t mapIndex = 0;
-        if (gPlayContext.shift1PNotes5KFor7KSkin)
-        {
-            mapIndex = gPlayContext.shift2PNotes5KFor7KSkin ? 3 : 2;
-        }
-        else
-        {
-            mapIndex = gPlayContext.shift2PNotes5KFor7KSkin ? 1 : 0;
-        }
-        _bombTimerMap = &bombTimer5k[mapIndex];  _bombLNTimerMap = &bombTimer5kLN[mapIndex];
+    case 10: {
+        _bombTimerMap = &bombTimer5k[fiveKeyMapIndex];
+        _bombLNTimerMap = &bombTimer5kLN[fiveKeyMapIndex];
         break;
     }
-    case 7:  
-    case 14: _bombTimerMap = &bombTimer7k;  _bombLNTimerMap = &bombTimer7kLN; break;
-    case 9:  _bombTimerMap = &bombTimer9k;  _bombLNTimerMap = &bombTimer9kLN; break;
+    case 7:
+    case 14:
+        _bombTimerMap = &bombTimer7k;
+        _bombLNTimerMap = &bombTimer7kLN;
+        break;
+    case 9:
+        _bombTimerMap = &bombTimer9k;
+        _bombLNTimerMap = &bombTimer9kLN;
+        break;
     default: break;
     }
 
@@ -277,35 +275,31 @@ RulesetBMS::RulesetBMS(std::shared_ptr<ChartFormatBase> format, std::shared_ptr<
     using namespace std::string_literals;
 
     _basic.health = health;
-    initGaugeParams(gauge);
+    initGaugeParams(mods.gauge);
 
     _side = side;
+    _judgeScratch = !(mods.assist_mask & PLAY_MOD_ASSIST_AUTOSCR);
 	switch (side)
 	{
 	case RulesetBMS::PlaySide::SINGLE:
         _k1P = true;
         _k2P = false;
-        _judgeScratch = !(gPlayContext.mods[PLAYER_SLOT_PLAYER].assist_mask & PLAY_MOD_ASSIST_AUTOSCR);
         break;
 	case RulesetBMS::PlaySide::DOUBLE:
 		_k1P = true;
 		_k2P = true;
-        _judgeScratch = !(gPlayContext.mods[PLAYER_SLOT_PLAYER].assist_mask & PLAY_MOD_ASSIST_AUTOSCR);
 		break;
 	case RulesetBMS::PlaySide::BATTLE_1P:
 		_k1P = true;
 		_k2P = false;
-        _judgeScratch = !(gPlayContext.mods[PLAYER_SLOT_PLAYER].assist_mask & PLAY_MOD_ASSIST_AUTOSCR);
 		break;
 	case RulesetBMS::PlaySide::BATTLE_2P:
 		_k1P = false;
 		_k2P = true;
-        _judgeScratch = !(gPlayContext.mods[PLAYER_SLOT_TARGET].assist_mask & PLAY_MOD_ASSIST_AUTOSCR);
 		break;
     default:
         _k1P = true;
         _k2P = true;
-        _judgeScratch = false;
         break;
 	}
 

src/game/ruleset/ruleset_bms_replay.cpp
@@ -1,68 +1,36 @@
 #include "ruleset_bms_replay.h"
-#include "game/scene/scene.h"
-#include "game/scene/scene_context.h"
 
 #include <cassert>
-#include <optional>
 #include <utility>
 
-RulesetBMSReplay::RulesetBMSReplay(
-    std::shared_ptr<ChartFormatBase> format_,
-    std::shared_ptr<ChartObjectBase> chart_,
-    std::shared_ptr<ReplayChart> replay_,
-    PlayModifierGaugeType gauge,
-    GameModeKeys keys,
-    JudgeDifficulty difficulty,
-    double health,
-    PlaySide side)
-    : RulesetBase(format_, chart_)
-    , RulesetBMS(std::move(format_), std::move(chart_), gauge, keys, difficulty, health, side)
-    , replay(std::move(replay_))
+RulesetBMSReplay::RulesetBMSReplay(std::shared_ptr<ChartFormatBase> format_, std::shared_ptr<ChartObjectBase> chart_,
+                                   std::shared_ptr<ReplayChart> replay_, const PlayModifiers mods, GameModeKeys keys,
+                                   JudgeDifficulty difficulty, double health, PlaySide side, const int fiveKeyMapIndex,
+                                   const double pitchSpeed)
+    : RulesetBase(format_, chart_),
+      RulesetBMS(std::move(format_), std::move(chart_), mods, keys, difficulty, health, side, fiveKeyMapIndex),
+      replay(std::move(replay_))
 {
+    assert(side == PlaySide::AUTO || side == PlaySide::AUTO_DOUBLE || side == PlaySide::AUTO_2P || side == PlaySide::RIVAL || side == PlaySide::MYBEST);
+
+    if (fiveKeyMapIndex == -1)
+    {
+        _inputDownMap = &REPLAY_CMD_INPUT_DOWN_MAP;
+        _inputUpMap = &REPLAY_CMD_INPUT_UP_MAP;
+    }
+    else
+    {
+        _inputDownMap = &REPLAY_CMD_INPUT_DOWN_MAP_5K[fiveKeyMapIndex];
+        _inputUpMap = &REPLAY_CMD_INPUT_UP_MAP_5K[fiveKeyMapIndex];
+    }
+
     itReplayCommand = replay->commands.begin();
     showJudge = (_side == PlaySide::AUTO || _side == PlaySide::AUTO_DOUBLE || _side == PlaySide::AUTO_2P);
-
-    if (gPlayContext.mode == SkinType::PLAY5 || gPlayContext.mode == SkinType::PLAY5_2)
-    {
-        if (gPlayContext.shift1PNotes5KFor7KSkin)
-        {
-            replayCmdMapIndex = gPlayContext.shift2PNotes5KFor7KSkin ? 3 : 2;
-        }
-        else
-        {
-            replayCmdMapIndex = gPlayContext.shift2PNotes5KFor7KSkin ? 1 : 0;
-        }
-    }
 
     if (replay->pitchValue != 0)
     {
         static const double tick = std::pow(2, 1.0 / 12);
-        playbackSpeed = std::pow(tick, replay->pitchValue);
-    }
-
-    switch (side)
-    {
-    case RulesetBMS::PlaySide::AUTO:
-    case RulesetBMS::PlaySide::AUTO_DOUBLE:
-        _judgeScratch = !(gPlayContext.mods[PLAYER_SLOT_PLAYER].assist_mask & PLAY_MOD_ASSIST_AUTOSCR);
-        break;
-
-    case RulesetBMS::PlaySide::AUTO_2P:
-        _judgeScratch = !(gPlayContext.mods[PLAYER_SLOT_TARGET].assist_mask & PLAY_MOD_ASSIST_AUTOSCR);
-        break;
-
-    case RulesetBMS::PlaySide::MYBEST:
-        _judgeScratch = !(gPlayContext.mods[PLAYER_SLOT_MYBEST].assist_mask & PLAY_MOD_ASSIST_AUTOSCR);
-        break;
-
-    case RulesetBMS::PlaySide::SINGLE:
-    case RulesetBMS::PlaySide::DOUBLE:
-    case RulesetBMS::PlaySide::BATTLE_1P:
-    case RulesetBMS::PlaySide::BATTLE_2P:
-    case RulesetBMS::PlaySide::RIVAL:
-    case RulesetBMS::PlaySide::NETWORK:
-        assert(false);
-        break;
+        replayTimestampMultiplier = std::pow(tick, replay->pitchValue) / pitchSpeed;
     }
 }
 
@@ -77,13 +45,11 @@ void RulesetBMSReplay::update(const lunaticvibes::Time& t)
 
     auto rt = t - _startTime.norm();
     _basic.play_time = rt;
-    using namespace chart;
 
     const InputMask prevKeyPressing = keyPressing;
-    while (itReplayCommand != replay->commands.end() && rt.norm() >= (long long)std::round(itReplayCommand->ms * playbackSpeed / gSelectContext.pitchSpeed))
+    while (itReplayCommand != replay->commands.end() && rt.norm() >= (long long)std::round(itReplayCommand->ms * replayTimestampMultiplier))
     {
         auto cmd = itReplayCommand->type;
-
         if (_side == PlaySide::AUTO_2P)
         {
             cmd = ReplayChart::Commands::leftSideCmdToRightSide(cmd);
@@ -91,28 +57,13 @@ void RulesetBMSReplay::update(const lunaticvibes::Time& t)
 
         if (!skipToEnd)
         {
-            if (gPlayContext.mode == SkinType::PLAY5 || gPlayContext.mode == SkinType::PLAY5_2)
+            if (auto it = _inputDownMap->find(cmd); it != _inputDownMap->end())
             {
-                if (auto it = REPLAY_CMD_INPUT_DOWN_MAP_5K[replayCmdMapIndex].find(cmd);
-                    it != REPLAY_CMD_INPUT_DOWN_MAP_5K[replayCmdMapIndex].end())
-                {
-                    keyPressing[it->second] = true;
-                }
-                else if (REPLAY_CMD_INPUT_UP_MAP_5K[replayCmdMapIndex].find(cmd) != REPLAY_CMD_INPUT_UP_MAP_5K[replayCmdMapIndex].end())
-                {
-                    keyPressing[REPLAY_CMD_INPUT_UP_MAP_5K[replayCmdMapIndex].at(cmd)] = false;
-                }
+                keyPressing[it->second] = true;
             }
-            else
+            else if (auto it = _inputUpMap->find(cmd); it != _inputUpMap->end())
             {
-                if (auto it = REPLAY_CMD_INPUT_DOWN_MAP.find(cmd); it != REPLAY_CMD_INPUT_DOWN_MAP.end())
-                {
-                    keyPressing[it->second] = true;
-                }
-                else if (REPLAY_CMD_INPUT_UP_MAP.find(cmd) != REPLAY_CMD_INPUT_UP_MAP.end())
-                {
-                    keyPressing[REPLAY_CMD_INPUT_UP_MAP.at(cmd)] = false;
-                }
+                keyPressing[it->second] = false;
             }
 
             switch (cmd)

src/game/scene/scene_play.cpp
@@ -899,24 +899,29 @@ bool ScenePlay::createRuleset()
 
             switch (objType)
             {
-            case BASE: 
+            case BASE:
                 return std::make_shared<RulesetBMS>(
-                gChartContext.chart, gPlayContext.chartObj[slot],
-                gPlayContext.mods[slot].gauge, keys, judgeDiff,
-                gPlayContext.initialHealth[slot], playSide);
+                    gChartContext.chart, gPlayContext.chartObj[slot], gPlayContext.mods[slot], keys, judgeDiff,
+                    gPlayContext.initialHealth[slot], playSide,
+                    gPlayContext.shiftFiveKeyForSevenKeyIndex(gPlayContext.mode == SkinType::PLAY5 ||
+                                                              gPlayContext.mode == SkinType::PLAY5_2));
 
             case AUTO:
                 return std::make_shared<RulesetBMSAuto>(
-                    gChartContext.chart, gPlayContext.chartObj[slot],
-                    gPlayContext.mods[slot].gauge, keys, judgeDiff,
-                    gPlayContext.initialHealth[slot], playSide);
+                    gChartContext.chart, gPlayContext.chartObj[slot], gPlayContext.mods[slot], keys, judgeDiff,
+                    gPlayContext.initialHealth[slot], playSide,
+                    gPlayContext.shiftFiveKeyForSevenKeyIndex(gPlayContext.mode == SkinType::PLAY5 ||
+                                                              gPlayContext.mode == SkinType::PLAY5_2));
 
             case REPLAY:
                 assert(gPlayContext.replayMybest != nullptr);
+                gPlayContext.mods[slot].gauge = gPlayContext.replayMybest->gaugeType;
                 return std::make_shared<RulesetBMSReplay>(
                     gChartContext.chartMybest, gPlayContext.chartObj[slot], gPlayContext.replayMybest,
-                    gPlayContext.replayMybest->gaugeType, keys, judgeDiff,
-                    gPlayContext.initialHealth[slot], playSide);
+                    gPlayContext.mods[slot], keys, judgeDiff, gPlayContext.initialHealth[slot], playSide,
+                    gPlayContext.shiftFiveKeyForSevenKeyIndex(gPlayContext.mode == SkinType::PLAY5 ||
+                                                              gPlayContext.mode == SkinType::PLAY5_2),
+                    gSelectContext.pitchSpeed);
 
             default:
                 return nullptr;

src/game/ruleset/ruleset_bms.h
@@ -209,14 +209,9 @@ protected:
 
 
 public:
-    RulesetBMS(
-        std::shared_ptr<ChartFormatBase> format,
-        std::shared_ptr<ChartObjectBase> chart,
-        PlayModifierGaugeType gauge,
-        GameModeKeys keys,
-        JudgeDifficulty difficulty = LR2_DEFAULT_RANK,
-        double health = 1.0,
-        PlaySide side = PlaySide::SINGLE);
+    // fiveKeyMapIndex - if not 5k, set to -1.
+    RulesetBMS(std::shared_ptr<ChartFormatBase> format, std::shared_ptr<ChartObjectBase> chart, PlayModifiers mods,
+               GameModeKeys keys, JudgeDifficulty difficulty, double health, PlaySide side, int fiveKeyMapIndex);
 
     void initGaugeParams(PlayModifierGaugeType gauge);
 

src/game/ruleset/ruleset_bms_auto.cpp
@@ -6,42 +6,15 @@
 #include "game/scene/scene_context.h"
 #include "game/chart/chart_types.h"
 
-RulesetBMSAuto::RulesetBMSAuto(
-    const std::shared_ptr<ChartFormatBase>& format,
-    const std::shared_ptr<ChartObjectBase>& chart,
-    PlayModifierGaugeType gauge,
-    GameModeKeys keys,
-    JudgeDifficulty difficulty,
-    double health,
-    PlaySide side) : RulesetBase(format, chart), RulesetBMS(format, chart, gauge, keys, difficulty, health, side)
+RulesetBMSAuto::RulesetBMSAuto(const std::shared_ptr<ChartFormatBase>& format,
+                               const std::shared_ptr<ChartObjectBase>& chart, const PlayModifiers mods,
+                               GameModeKeys keys, JudgeDifficulty difficulty, double health, PlaySide side,
+                               const int fiveKeyMapIndex)
+    : RulesetBase(format, chart), RulesetBMS(format, chart, mods, keys, difficulty, health, side, fiveKeyMapIndex)
 {
     assert(side == PlaySide::AUTO || side == PlaySide::AUTO_DOUBLE || side == PlaySide::AUTO_2P || side == PlaySide::RIVAL);
-
     showJudge = (_side == PlaySide::AUTO || _side == PlaySide::AUTO_DOUBLE || _side == PlaySide::AUTO_2P);
-
     isPressingLN.fill(false);
-
-    switch (side)
-    {
-    case RulesetBMS::PlaySide::AUTO:
-    case RulesetBMS::PlaySide::AUTO_DOUBLE:
-        _judgeScratch = !(gPlayContext.mods[PLAYER_SLOT_PLAYER].assist_mask & PLAY_MOD_ASSIST_AUTOSCR);
-        break;
-
-    case RulesetBMS::PlaySide::AUTO_2P:
-    case RulesetBMS::PlaySide::RIVAL:
-        _judgeScratch = !(gPlayContext.mods[PLAYER_SLOT_TARGET].assist_mask & PLAY_MOD_ASSIST_AUTOSCR);
-        break;
-
-    case RulesetBMS::PlaySide::SINGLE:
-    case RulesetBMS::PlaySide::DOUBLE:
-    case RulesetBMS::PlaySide::BATTLE_1P:
-    case RulesetBMS::PlaySide::BATTLE_2P:
-    case RulesetBMS::PlaySide::MYBEST:
-    case RulesetBMS::PlaySide::NETWORK:
-        assert(false);
-    }
-
     setTargetRate(1.0);
 }
 

src/game/ruleset/ruleset_bms_auto.h
@@ -6,14 +6,10 @@
 class RulesetBMSAuto : public RulesetBMS
 {
 public:
-    RulesetBMSAuto(
-        const std::shared_ptr<ChartFormatBase>& format,
-        const std::shared_ptr<ChartObjectBase>& chart,
-        PlayModifierGaugeType gauge,
-        GameModeKeys keys,
-        JudgeDifficulty difficulty = LR2_DEFAULT_RANK,
-        double health = 1.0,
-        PlaySide side = PlaySide::AUTO);
+    // fiveKeyMapIndex - if not 5k, set to -1.
+    RulesetBMSAuto(const std::shared_ptr<ChartFormatBase> &format, const std::shared_ptr<ChartObjectBase> &chart,
+                   PlayModifiers mods, GameModeKeys keys, JudgeDifficulty difficulty, double health, PlaySide side,
+                   int fiveKeyMapIndex);
 
 protected:
 	double targetRate = 100.0;

src/game/ruleset/ruleset_bms_network.cpp
@@ -7,8 +7,10 @@
 #include "common/log.h"
 #include "game/scene/scene_context.h"
 
-RulesetBMSNetwork::RulesetBMSNetwork(GameModeKeys keys, unsigned playerIndex) : 
-    RulesetBase(nullptr, nullptr), RulesetBMS(nullptr, nullptr, PlayModifierGaugeType::NORMAL, keys, JudgeDifficulty::NORMAL, 1.0, PlaySide::NETWORK), playerIndex(playerIndex)
+RulesetBMSNetwork::RulesetBMSNetwork(GameModeKeys keys, unsigned playerIndex)
+    : RulesetBase(nullptr, nullptr),
+      RulesetBMS(nullptr, nullptr, PlayModifiers{}, keys, JudgeDifficulty::NORMAL, 1.0, PlaySide::NETWORK, -1),
+      playerIndex(playerIndex)
 {
     showJudge = false;
     _judgeScratch = false;

commit 89550707cd293ca45666546b64628268e411ee91

fix(game): pass fiveKeyMapIndex to ruleset for 10k tooo
Fixes https://github.com/chown2/lunaticvibesf/issues/88 (10k charts
crash).

Fixes: ebb5cd061f64 ("refactor(game): don't depend on global state in rulesets")

src/game/chart/chart.cpp
@@ -5,6 +5,7 @@
 #include "common/chartformat/chartformat_types.h"
 #include "game/runtime/state.h"
 #include "game/scene/scene_context.h"
+#include <iterator>
 
 
 using namespace chart;
@@ -89,7 +90,7 @@ const std::vector<Input::Pad>& chart::LaneToKey(int keys, size_t idx)
 // from Input::Pad to NoteLaneIndex
 chart::NoteLaneIndex chart::KeyToLane(int keys, Input::Pad pad)
 {
-    assert(pad < 30);
+    assert(pad < Input::Pad::LANE_COUNT);
 
     using namespace chart;
     switch (keys)
@@ -97,7 +98,7 @@ chart::NoteLaneIndex chart::KeyToLane(int keys, Input::Pad pad)
     case 5:
     case 10:
     {
-        static const NoteLaneIndex lane[4][30] =
+        static constexpr NoteLaneIndex lane[4][Input::Pad::LANE_COUNT] =
         {
             {
                 Sc1, Sc1, N11, N12, N13, N14, N15, _, _, _, _, _, _, _, _,
@@ -116,12 +117,15 @@ chart::NoteLaneIndex chart::KeyToLane(int keys, Input::Pad pad)
                 Sc2, Sc2, _, _, N21, N22, N23, N24, N25, _, _, _, _, _, _,
             },
         };
-        return lane[gPlayContext.shiftFiveKeyForSevenKeyIndex(true)][pad];
+        const auto fiveKeyMapIndex = gPlayContext.shiftFiveKeyForSevenKeyIndex(true);
+        assert(fiveKeyMapIndex >= 0);
+        assert(fiveKeyMapIndex < static_cast<int>(std::size(lane)));
+        return lane[fiveKeyMapIndex][pad];
     }
     case 7:
     case 14:
     {
-        static const NoteLaneIndex lane[] =
+        static constexpr NoteLaneIndex lane[Input::Pad::LANE_COUNT] =
         {
             Sc1, Sc1, N11, N12, N13, N14, N15, N16, N17, _, _, _, _, _, _,
             Sc2, Sc2, N21, N22, N23, N24, N25, N26, N27, _, _, _, _, _, _,
@@ -130,7 +134,7 @@ chart::NoteLaneIndex chart::KeyToLane(int keys, Input::Pad pad)
     }
     case 9:
     {
-        static const NoteLaneIndex lane[] =
+        static constexpr NoteLaneIndex lane[Input::Pad::LANE_COUNT] =
         {
             _, _, N11, N12, N13, N14, N15, N16, N17, N18, N19, _, _, _, _,
             _, _, _, _, _, _, _, _, _, _, _, _, _, _, _,

src/game/ruleset/ruleset_bms.cpp
@@ -1,5 +1,6 @@
 #include "ruleset_bms.h"
 
+#include <iterator>
 #include <utility>
 
 #include "game/arena/arena_data.h"
@@ -241,6 +242,9 @@ RulesetBMS::RulesetBMS(std::shared_ptr<ChartFormatBase> format, std::shared_ptr<
     {
     case 5:
     case 10: {
+        assert(fiveKeyMapIndex >= 0);
+        assert(fiveKeyMapIndex < static_cast<int>(std::size(bombTimer5k)));
+        assert(fiveKeyMapIndex < static_cast<int>(std::size(bombTimer5kLN)));
         _bombTimerMap = &bombTimer5k[fiveKeyMapIndex];
         _bombLNTimerMap = &bombTimer5kLN[fiveKeyMapIndex];
         break;

src/game/ruleset/ruleset_bms_replay.cpp
@@ -1,6 +1,7 @@
 #include "ruleset_bms_replay.h"
 
 #include <cassert>
+#include <iterator>
 #include <utility>
 
 RulesetBMSReplay::RulesetBMSReplay(std::shared_ptr<ChartFormatBase> format_, std::shared_ptr<ChartObjectBase> chart_,
@@ -20,6 +21,9 @@ RulesetBMSReplay::RulesetBMSReplay(std::shared_ptr<ChartFormatBase> format_, std
     }
     else
     {
+        assert(fiveKeyMapIndex >= 0);
+        assert(fiveKeyMapIndex < static_cast<int>(std::size(REPLAY_CMD_INPUT_DOWN_MAP_5K)));
+        assert(fiveKeyMapIndex < static_cast<int>(std::size(REPLAY_CMD_INPUT_UP_MAP_5K)));
         _inputDownMap = &REPLAY_CMD_INPUT_DOWN_MAP_5K[fiveKeyMapIndex];
         _inputUpMap = &REPLAY_CMD_INPUT_UP_MAP_5K[fiveKeyMapIndex];
     }

src/game/scene/scene_play.cpp
@@ -903,15 +903,13 @@ bool ScenePlay::createRuleset()
                 return std::make_shared<RulesetBMS>(
                     gChartContext.chart, gPlayContext.chartObj[slot], gPlayContext.mods[slot], keys, judgeDiff,
                     gPlayContext.initialHealth[slot], playSide,
-                    gPlayContext.shiftFiveKeyForSevenKeyIndex(gPlayContext.mode == SkinType::PLAY5 ||
-                                                              gPlayContext.mode == SkinType::PLAY5_2));
+                    gPlayContext.shiftFiveKeyForSevenKeyIndex(keys == 5 || keys == 10));
 
             case AUTO:
                 return std::make_shared<RulesetBMSAuto>(
                     gChartContext.chart, gPlayContext.chartObj[slot], gPlayContext.mods[slot], keys, judgeDiff,
                     gPlayContext.initialHealth[slot], playSide,
-                    gPlayContext.shiftFiveKeyForSevenKeyIndex(gPlayContext.mode == SkinType::PLAY5 ||
-                                                              gPlayContext.mode == SkinType::PLAY5_2));
+                    gPlayContext.shiftFiveKeyForSevenKeyIndex(keys == 5 || keys == 10));
 
             case REPLAY:
                 assert(gPlayContext.replayMybest != nullptr);
@@ -919,9 +917,7 @@ bool ScenePlay::createRuleset()
                 return std::make_shared<RulesetBMSReplay>(
                     gChartContext.chartMybest, gPlayContext.chartObj[slot], gPlayContext.replayMybest,
                     gPlayContext.mods[slot], keys, judgeDiff, gPlayContext.initialHealth[slot], playSide,
-                    gPlayContext.shiftFiveKeyForSevenKeyIndex(gPlayContext.mode == SkinType::PLAY5 ||
-                                                              gPlayContext.mode == SkinType::PLAY5_2),
-                    gSelectContext.pitchSpeed);
+                    gPlayContext.shiftFiveKeyForSevenKeyIndex(keys == 5 || keys == 10), gSelectContext.pitchSpeed);
 
             default:
                 return nullptr;
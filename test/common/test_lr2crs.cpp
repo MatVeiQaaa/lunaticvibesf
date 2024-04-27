MatVeiQaaa/lunaticvibesf

commit 21e1e22d854120514698e67c7bd1789ef3e3022a

refactor(common): interpret lr2crs fields in case-sensitively

src/common/coursefile/lr2crs.cpp
@@ -58,13 +58,14 @@ CourseLr2crs::CourseLr2crs(const Path& filePath)
 			Course& c = courses.back();
 			for (auto& [name, value] : course.second)
 			{
-				if (lunaticvibes::iequals(name, "title"))
+				// NOTE: LR2 parses fields case-sensitively.
+				if (name == "title")
 					c.title = value.data();
-				else if (lunaticvibes::iequals(name, "line"))
+				else if (name == "line")
 					c.line = toInt(value.data());
-				else if (lunaticvibes::iequals(name, "type"))
+				else if ("type")
 					c.type = decltype(c.type)(toInt(value.data()));
-				else if (lunaticvibes::iequals(name, "hash"))
+				else if ("hash")
 				{
 					// first 32 characters are course metadata, of which we didn't make use; the 10th char is course type, the last 4 are assumed to be uploader ID
 					// after that, each 32 chars indicates a chart MD5

commit e8e0479ab82ad8f65c43f82d242290f8fb46b016

fix(common): actually parse LR2 course 'type' and 'hash'
And add tests!

Fixes https://github.com/chown2/lunaticvibesf/issues/94 ([Course] Song
charts not appearing to prevent crash).

Fixes: 21e1e22d8541 ("refactor(common): interpret lr2crs fields in case-sensitively")

src/common/coursefile/lr2crs.cpp
@@ -1,7 +1,9 @@
 #include "lr2crs.h"
 
+#include <exception>
 #include <fstream>
 #include <sstream>
+
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
 
@@ -11,7 +13,7 @@
 #include "common/utils.h"
 #include "common/sysutil.h"
 
-CourseLr2crs::CourseLr2crs(const Path& filePath)
+std::stringstream readIntoUtf8StringStream(const Path& filePath)
 {
 	// .lr2crs is a standard xml file. A file may contain muiltiple courses.
 
@@ -20,14 +22,13 @@ CourseLr2crs::CourseLr2crs(const Path& filePath)
 	if (ifs.fail())
 	{
 		LOG_WARNING << "[lr2crs] " << filePath << " File ERROR";
-		return;
+		return {};
 	}
+
 	std::stringstream ss;
 	ss << ifs.rdbuf();
 	ss.sync();
 	ifs.close();
-
-	addTime = getFileLastWriteTime(filePath);
 
 	// convert codepage
 	auto encoding = getFileEncoding(ss);
@@ -42,6 +43,16 @@ CourseLr2crs::CourseLr2crs(const Path& filePath)
 	ssUTF8.sync();
 	ss.clear();
 
+	return ssUTF8;
+}
+
+CourseLr2crs::CourseLr2crs(const Path& filePath)
+	: CourseLr2crs(filePath.filename().u8string(), getFileLastWriteTime(filePath), readIntoUtf8StringStream(filePath))
+{
+}
+
+CourseLr2crs::CourseLr2crs(std::string_view source, long long addTime, std::stringstream ssUTF8) : addTime(addTime)
+{
 	// parse as XML
 	try
 	{
@@ -50,8 +61,7 @@ CourseLr2crs::CourseLr2crs(const Path& filePath)
 		pt::read_xml(ssUTF8, tree);
 		for (auto& course : tree.get_child("courselist"))
 		{
-			courses.emplace_back();
-			Course& c = courses.back();
+			Course& c = courses.emplace_back();
 			for (auto& [name, value] : course.second)
 			{
 				// NOTE: LR2 parses fields case-sensitively.
@@ -59,9 +69,9 @@ CourseLr2crs::CourseLr2crs(const Path& filePath)
 					c.title = value.data();
 				else if (name == "line")
 					c.line = toInt(value.data());
-				else if ("type")
+				else if (name == "type")
 					c.type = decltype(c.type)(toInt(value.data()));
-				else if ("hash")
+				else if (name == "hash")
 				{
 					// first 32 characters are course metadata, of which we didn't make use; the 10th char is course type, the last 4 are assumed to be uploader ID
 					// after that, each 32 chars indicates a chart MD5
@@ -72,17 +82,26 @@ CourseLr2crs::CourseLr2crs(const Path& filePath)
 						c.chartHash.emplace_back(hash.substr(count * 32, 32));
 					}
 				}
+				else
+				{
+					LOG_WARNING << "[CourseLr2crs] Unknown field type " << name;
+				}
 			}
 		}
 	}
-	catch (...)
+	catch (const std::exception& e)
 	{
+		LOG_WARNING << "[CourseLr2crs] Exception while parsing courses: " << e.what();
 		courses.clear();
 	}
+
+	LOG_DEBUG << "[CourseLr2crs] Loaded " << courses.size() << " courses from " << source;
 }
 
 HashMD5 CourseLr2crs::Course::getCourseHash() const
 {
+	// NOTE: do *not* change this, or old Lunatic Vibes course scores in the DB will get broken.
+
 	std::stringstream ss;
 	ss << (int)type;
 	ss << chartHash.size();

src/common/coursefile/lr2crs.h
@@ -1,5 +1,8 @@
 #pragma once
+
+#include <sstream>
 #include <string>
+#include <string_view>
 #include <vector>
 
 #include "common/types.h"
@@ -26,6 +29,8 @@ public:
 	long long addTime = 0;
 
 public:
+	// Delegates to the other constructor.
 	CourseLr2crs(const Path& filePath);
+	CourseLr2crs(std::string_view source, long long addTime, std::stringstream ssUTF8);
 	~CourseLr2crs() = default;
 };

test/CMakeLists.txt
@@ -11,6 +11,7 @@ add_executable(apptest
     common/test_fraction.cpp
     common/test_chartformat_bms.cpp
     common/test_hash.cpp
+    common/test_lr2crs.cpp
     common/test_path.cpp
     db/test_score_db.cpp
     db/test_song_db.cpp

test/common/test_lr2crs.cpp
@@ -1,0 +1,41 @@
+#include <sstream>
+#include <utility>
+
+#include <gmock/gmock.h>
+
+#include "common/coursefile/lr2crs.h"
+#include "common/hash.h"
+
+TEST(Lr2crs, ParsesSuccessfully)
+{
+    static constexpr long long add_time{12345};
+
+    std::stringstream ss;
+    ss << u8R"(<?xml version="1.0" encoding="shift_jis"?>
+<!--曲は上から順に登録されます。-->
+<courselist>
+
+	<course>
+		<title>GENOSIDE 2018 段位認定 発狂初段</Title>
+		<line>7</line>
+		<hash>00000000002000000000000000005190f872dd65dd08638b06d80470a3233fb91b72e8f6439a698e78f94be16470b7892371263af3b0d644fba62526c9f494818a8a6c2f3511eb0876a6c9a2027d7bbe</hash>
+		<type>2</type>
+	</course>
+
+</courselist>)";
+
+    CourseLr2crs lr2crs("test.lr2crs", add_time, std::move(ss));
+    EXPECT_EQ(lr2crs.addTime, add_time);
+    ASSERT_EQ(lr2crs.courses.size(), 1);
+    const auto& course = lr2crs.courses[0];
+    EXPECT_EQ(course.title, u8"GENOSIDE 2018 段位認定 発狂初段");
+    EXPECT_EQ(course.line, 7);
+    EXPECT_EQ(course.type, CourseLr2crs::Course::COURSE_GRADE);
+    EXPECT_EQ(course.hashTop, "00000000002000000000000000005190");
+    ASSERT_EQ(course.chartHash.size(), 4);
+    EXPECT_EQ(course.chartHash[0], HashMD5{"f872dd65dd08638b06d80470a3233fb9"});
+    EXPECT_EQ(course.chartHash[1], HashMD5{"1b72e8f6439a698e78f94be16470b789"});
+    EXPECT_EQ(course.chartHash[2], HashMD5{"2371263af3b0d644fba62526c9f49481"});
+    EXPECT_EQ(course.chartHash[3], HashMD5{"8a8a6c2f3511eb0876a6c9a2027d7bbe"});
+    EXPECT_EQ(course.getCourseHash(), HashMD5{"fe4c64fd329a55bc2499be2014a1f13a"});
+}
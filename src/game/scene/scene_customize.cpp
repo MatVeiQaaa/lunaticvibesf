MatVeiQaaa/lunaticvibesf

commit 5a34a33ad27e47c246b31ad8e8e8d3ce4786fbe4

refactor(game): refactor SkinMgr

src/game/scene/scene_customize.cpp
@@ -9,7 +9,8 @@
 #include "game/sound/soundset_lr2.h"
 #include "scene_context.h"
 
-SceneCustomize::SceneCustomize() : SceneBase(SkinType::THEME_SELECT, 240), _state(lunaticvibes::CustomizeState::Start)
+SceneCustomize::SceneCustomize(const std::shared_ptr<SkinMgr>& skinMgr)
+    : SceneBase(skinMgr, SkinType::THEME_SELECT, 240), _skinMgr(skinMgr), _state(lunaticvibes::CustomizeState::Start)
 {
     _type = SceneType::CUSTOMIZE;
 
@@ -194,10 +195,7 @@ void SceneCustomize::updateMain()
         {
             if (!gInCustomize || selectedMode != SkinType::MUSIC_SELECT)
             {
-                if (SkinMgr::get(selectedMode))
-                {
-                    SkinMgr::unload(selectedMode);
-                }
+                _skinMgr->unload(selectedMode);
             }
             load(selectedMode);
         }
@@ -259,7 +257,7 @@ void SceneCustomize::updateMain()
             if (skinList[selectedMode].size() > 1)
             {
                 int selectedIdx;
-                const auto currentSkin = SkinMgr::get(selectedMode);
+                const auto currentSkin = _skinMgr->get(selectedMode);
                 for (selectedIdx = 0; selectedIdx < (int)skinList[selectedMode].size(); selectedIdx++)
                 {
                     const Path& p1 = skinList[selectedMode][selectedIdx];
@@ -323,10 +321,7 @@ void SceneCustomize::updateMain()
                     }
 
                     pSkin->setHandleMouseEvents(false);
-                    if (SkinMgr::get(selectedMode))
-                    {
-                        SkinMgr::unload(selectedMode);
-                    }
+                    _skinMgr->unload(selectedMode);
                     load(selectedMode);
                     pSkin->setHandleMouseEvents(true);
 
@@ -407,10 +402,7 @@ void SceneCustomize::updateFadeout()
     if (rt.norm() > pSkin->info.timeOutro)
     {
         pSkin->setHandleMouseEvents(false);
-        if (SkinMgr::get(selectedMode))
-        {
-            SkinMgr::unload(selectedMode);
-        }
+        _skinMgr->unload(selectedMode);
         gNextScene = SceneType::SELECT;
         gExitingCustomize = true;
     }
@@ -513,9 +505,9 @@ void SceneCustomize::load(SkinType mode)
     }
     else
     {
-        if (!SkinMgr::get(mode))
-            SkinMgr::load(mode, true);
-        std::shared_ptr<SkinBase> ps = SkinMgr::get(mode);
+        if (!_skinMgr->get(mode))
+            _skinMgr->reload(mode, true);
+        std::shared_ptr<SkinBase> ps = _skinMgr->get(mode);
         optionsMap.clear();
         optionsKeyList.clear();
 
@@ -598,7 +590,7 @@ void SceneCustomize::save(SkinType mode) const
     }
     else
     {
-        std::shared_ptr<SkinBase> ps = SkinMgr::get(mode);
+        std::shared_ptr<SkinBase> ps = _skinMgr->get(mode);
         if (ps != nullptr)
         {
             pCustomize = ConfigMgr::Profile()->getPath() / "customize";

src/game/scene/scene_select.cpp
@@ -350,8 +350,10 @@ void config_fx()
 
 std::shared_ptr<SceneCustomize> SceneSelect::_virtualSceneCustomize = nullptr;
 
-SceneSelect::SceneSelect() : SceneBase(SkinType::MUSIC_SELECT, 250)
+SceneSelect::SceneSelect(const std::shared_ptr<SkinMgr>& skinMgr)
+    : SceneBase(skinMgr, SkinType::MUSIC_SELECT, 250), _skinMgr(skinMgr)
 {
+    assert(pSkin != nullptr);
     _type = SceneType::SELECT;
 
     _inputAvailable = INPUT_MASK_FUNC | INPUT_MASK_MOUSE;
@@ -930,7 +932,7 @@ void SceneSelect::_updateAsync()
             createNotification("Loading skin options...");
 
             pSkin->setHandleMouseEvents(false);
-            _virtualSceneCustomize = std::make_shared<SceneCustomize>();
+            _virtualSceneCustomize = std::make_shared<SceneCustomize>(_skinMgr);
             _virtualSceneCustomize->setIsVirtual(true);
             _virtualSceneCustomize->loopStart();
             pSkin->setHandleMouseEvents(true);

src/game/scene/scene_select.h
@@ -26,6 +26,10 @@ class ChartFormatBase;
 class SceneCustomize;
 class SceneSelect : public SceneBase
 {
+public:
+    explicit SceneSelect(const std::shared_ptr<SkinMgr>& skinMgr);
+    ~SceneSelect() override;
+
 private:
     eSelectState state;
     InputMask _inputAvailable;
@@ -67,6 +71,7 @@ private:
 
     // virtual Customize scene, customize option toggle in select scene support
     static std::shared_ptr<SceneCustomize> _virtualSceneCustomize;
+    std::shared_ptr<SkinMgr> _skinMgr;
 
     // smooth scrolling
     lunaticvibes::Time scrollButtonTimestamp;
@@ -92,9 +97,6 @@ private:
     bool imguiShow = false;
 
 public:
-    SceneSelect();
-    ~SceneSelect() override;
-
     void closeReadme(const lunaticvibes::Time&);
     void openChartReadme(const lunaticvibes::Time&);
     void openHelpFile(const lunaticvibes::Time&, size_t idx);

src/game/game.cpp
@@ -276,7 +276,6 @@ int main(int argc, char* argv[])
 
     SoundMgr::stopUpdate();
 
-    SkinMgr::clean();
     graphics_free();
 
     ImGui::DestroyContext();
@@ -297,6 +296,7 @@ void mainLoop()
 
     SceneType currentScene = SceneType::NOT_INIT;
 
+    auto skinMgr = std::make_shared<SkinMgr>();
     std::shared_ptr<SceneBase> scene;
     std::shared_ptr<SceneBase> sceneCustomize;
     while (currentScene != SceneType::EXIT && gNextScene != SceneType::EXIT)
@@ -313,7 +313,7 @@ void mainLoop()
         {
             if (sceneCustomize == nullptr)
             {
-                sceneCustomize = lunaticvibes::buildScene(SceneType::CUSTOMIZE);
+                sceneCustomize = lunaticvibes::buildScene(skinMgr, SceneType::CUSTOMIZE);
                 sceneCustomize->loopStart();
                 sceneCustomize->inputLoopStart();
             }
@@ -347,7 +347,7 @@ void mainLoop()
                 State::set(IndexTimer::_LOAD_START, TIMER_NEVER);
                 State::set(IndexTimer::PLAY_READY, TIMER_NEVER);
                 State::set(IndexTimer::PLAY_START, TIMER_NEVER);
-                scene = lunaticvibes::buildScene(currentScene);
+                scene = lunaticvibes::buildScene(skinMgr, currentScene);
                 assert(scene != nullptr);
                 lunaticvibes::Time t;
                 State::set(IndexTimer::SCENE_START, t.norm());

src/game/scene/scene.cpp
@@ -1,22 +1,25 @@
 #include "scene.h"
+
+#include <memory>
+
 #include "common/beat.h"
+#include "common/log.h"
 #include "common/sysutil.h"
-#include "game/graphics/graphics.h"
-#include "game/runtime/state.h"
-#include "game/runtime/generic_info.h"
-#include "game/skin/skin_mgr.h"
-#include "scene_context.h"
 #include "config/config_mgr.h"
-
-#include "imgui.h"
-#include "game/skin/skin_lr2_debug.h"
+#include "game/graphics/graphics.h"
+#include "game/runtime/generic_info.h"
 #include "game/runtime/i18n.h"
-
+#include "game/runtime/state.h"
+#include "game/skin/skin_lr2_debug.h"
+#include "game/skin/skin_mgr.h"
 #include "game/sound/sound_mgr.h"
 #include "game/sound/sound_sample.h"
+#include "scene_context.h"
+
+#include <imgui.h>
 
 // prototype
-SceneBase::SceneBase(SkinType skinType, unsigned rate, bool backgroundInput)
+SceneBase::SceneBase(const std::shared_ptr<SkinMgr>& skinMgr, SkinType skinType, unsigned rate, bool backgroundInput)
     : AsyncLooper("UpdateLoop", std::bind(&SceneBase::_updateAsync1, this), rate), _type(SceneType::NOT_INIT),
       _input(1000, backgroundInput)
 {
@@ -28,9 +31,15 @@ SceneBase::SceneBase(SkinType skinType, unsigned rate, bool backgroundInput)
 
     // Disable skin caching for now. dst options are changing all the time
     const bool simple_skin = gInCustomize && skinType != SkinType::THEME_SELECT;
-    SkinMgr::unload(skinType);
-    SkinMgr::load(skinType, gInCustomize && simple_skin);
-    pSkin = SkinMgr::get(skinType);
+    if (skinMgr)
+    {
+        skinMgr->reload(skinType, gInCustomize && simple_skin);
+        pSkin = skinMgr->get(skinType);
+    }
+    else
+    {
+        LOG_VERBOSE << "[SceneBase] No skinMgr";
+    }
 
     int notificationPosY = 480;
     int notificationWidth = 640;

src/game/scene/scene.h
@@ -6,6 +6,7 @@
 #include "game/runtime/state.h"
 #include "game/input/input_wrapper.h"
 #include "common/types.h"
+#include "game/skin/skin_mgr.h"
 
 enum class SceneType
 {
@@ -78,7 +79,8 @@ protected:
 
 public:
     SceneBase() = delete;
-    SceneBase(SkinType skinType, unsigned rate = 240, bool backgroundInput = false);
+    SceneBase(const std::shared_ptr<SkinMgr>& skinMgr, SkinType skinType, unsigned rate = 240,
+              bool backgroundInput = false);
     ~SceneBase() override;
     void inputLoopStart() { _input.loopStart(); }
     void inputLoopEnd() { _input.loopEnd(); }

src/game/scene/scene_course_result.cpp
@@ -10,11 +10,10 @@
 #include "config/config_mgr.h"
 #include <boost/algorithm/string.hpp>
 
-SceneCourseResult::SceneCourseResult() : SceneBase(SkinType::COURSE_RESULT, 1000)
+SceneCourseResult::SceneCourseResult(const std::shared_ptr<SkinMgr>& skinMgr)
+    : SceneBase(skinMgr, SkinType::COURSE_RESULT, 1000), _inputAvailable(INPUT_MASK_FUNC)
 {
     _type = SceneType::COURSE_RESULT;
-
-    _inputAvailable = INPUT_MASK_FUNC;
 
     if (gPlayContext.chartObj[PLAYER_SLOT_PLAYER] != nullptr)
     {

src/game/scene/scene_course_result.h
@@ -1,7 +1,9 @@
 #pragma once
-#include <mutex>
 #include "game/ruleset/ruleset.h"
+#include "game/skin/skin_mgr.h"
 #include "scene.h"
+#include <memory>
+#include <mutex>
 
 class ScoreBase;
 enum class eCourseResultState
@@ -14,6 +16,10 @@ enum class eCourseResultState
 
 class SceneCourseResult : public SceneBase
 {
+public:
+    explicit SceneCourseResult(const std::shared_ptr<SkinMgr>& skinMgr);
+    ~SceneCourseResult() override;
+
 private:
     eCourseResultState state;
     InputMask _inputAvailable;
@@ -44,10 +50,6 @@ protected:
     };
     std::map<SummaryArgs, unsigned> summary;
     double acc = 0.;
-
-public:
-    SceneCourseResult();
-    ~SceneCourseResult() override;
 
 protected:
     // Looper callbacks

commit e6645fd02eedeac1c7e3d3180799e25ca99f853b

fix(game): make virtual customize scene non-static
1) (?) Fixes ConfigMgr use-after-free after SkinMgr refactoring.
   Got it once and couldn't reproduce, but this seems to be the likely
   culprit.
2) Fixes a FIXME about things being shown in customize mode after
   exiting customize scene.

Don't squash because technically unrelated but:
Fixes: 5a34a33ad27e ("refactor(game): refactor SkinMgr")

src/game/scene/scene_customize.cpp
@@ -83,13 +83,15 @@ SceneCustomize::SceneCustomize(const std::shared_ptr<SkinMgr>& skinMgr)
 SceneCustomize::~SceneCustomize()
 {
     // Reset option entry names to avoid showing garbage in select skin customize mode.
-    // FIXME: if virtual customize scene exists but is disabled, when we exit real customize scene customize mode kicks
-    // up and refills texts with data again.
-    optionsKeyList.clear();
-    optionsMap.clear();
-    updateTexts();
-    State::set(IndexText::SKIN_NAME, "");
-    State::set(IndexText::SKIN_MAKER_NAME, "");
+    // Don't do it for the virtual scene as it is destructed after normal SceneCustomize is created.
+    if (!_is_virtual)
+    {
+        optionsKeyList.clear();
+        optionsMap.clear();
+        updateTexts();
+        State::set(IndexText::SKIN_NAME, "");
+        State::set(IndexText::SKIN_MAKER_NAME, "");
+    }
 
     // NOTE: do *not* call this here, as it may unintentionally load another skin which may wait for a callback from
     // main thread, causing a dead lock.
@@ -453,6 +455,7 @@ void SceneCustomize::setOption(size_t idxOption, size_t idxEntry)
 
 void SceneCustomize::load(SkinType mode)
 {
+    LOG_DEBUG << "[Customize] Load";
     StringPath configFilePath;
     if (mode == SkinType::SOUNDSET)
     {
@@ -521,6 +524,7 @@ void SceneCustomize::load(SkinType mode)
         }
         else
         {
+            LOG_DEBUG << "[Customize] ps==nullptr";
             State::set(IndexText::SKIN_NAME, "");
             State::set(IndexText::SKIN_MAKER_NAME, "");
         }

src/game/scene/scene_select.cpp
@@ -348,8 +348,6 @@ void config_fx()
 
 ////////////////////////////////////////////////////////////////////////////////
 
-std::shared_ptr<SceneCustomize> SceneSelect::_virtualSceneCustomize = nullptr;
-
 SceneSelect::SceneSelect(const std::shared_ptr<SkinMgr>& skinMgr)
     : SceneBase(skinMgr, SkinType::MUSIC_SELECT, 250), _skinMgr(skinMgr)
 {
@@ -489,11 +487,6 @@ SceneSelect::SceneSelect(const std::shared_ptr<SkinMgr>& skinMgr)
     loadLR2Sound();
 
     gCustomizeContext.modeUpdate = false;
-    if (_virtualSceneCustomize != nullptr)
-    {
-        _virtualSceneCustomize->loopStart();
-    }
-
     if (!gInCustomize)
     {
         SoundMgr::stopNoteSamples();
@@ -522,10 +515,7 @@ SceneSelect::~SceneSelect()
     if (_virtualSceneCustomize != nullptr)
     {
         _virtualSceneCustomize->loopEnd();
-        if (gNextScene == SceneType::CUSTOMIZE || gNextScene == SceneType::EXIT_TRANS || gNextScene == SceneType::EXIT)
-        {
-            _virtualSceneCustomize.reset();
-        }
+        _virtualSceneCustomize.reset();
     }
 
     postStartPreview();

src/game/scene/scene_select.h
@@ -70,7 +70,7 @@ private:
     std::array<size_t, 128> _keySampleIdxBuf{};
 
     // virtual Customize scene, customize option toggle in select scene support
-    static std::shared_ptr<SceneCustomize> _virtualSceneCustomize;
+    std::shared_ptr<SceneCustomize> _virtualSceneCustomize;
     std::shared_ptr<SkinMgr> _skinMgr;
 
     // smooth scrolling
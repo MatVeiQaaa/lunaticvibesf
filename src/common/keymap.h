MatVeiQaaa/lunaticvibesf

commit 1c5a5c247bd7e607169f7d813595393d328306fb

fix: interpret NONUS_BACKSLASH and NONUS_HASH
Fixes https://github.com/chown2/lunaticvibesf/issues/133 "Support for
ISO keyboards".

src/common/keymap.h
@@ -226,12 +226,16 @@ enum class Keyboard : unsigned
     K_JP_CONVERT,
     K_JP_KANA,
 
+    // ISO
+    K_NONUS_BACKSLASH,
+    K_NONUS_HASH,
+
     // scancodes with mask (e0)
     K_NUM_SLASH,
     K_NUM_STAR,
     K_NUM_ENTER,
 
-    K_COUNT
+    K_COUNT,
 };
 inline constexpr size_t keyboardKeyCount = static_cast<size_t>(Keyboard::K_COUNT);
 
@@ -366,6 +370,10 @@ inline const char* keyboardNameMap[keyboardKeyCount]{
     "JP_NOCONVERT",
     "JP_CONVERT",
     "JP_KANA",
+
+    // ISO
+    "NONUS_BACKSLASH",
+    "NONUS_HASH",
 };
 
 enum class JoystickAxis

src/game/graphics/SDL2/input.cpp
@@ -1,5 +1,7 @@
 #include "input.h"
 
+#include <common/assert.h>
+
 #include <SDL_scancode.h>
 
 unsigned char sdl_key_from_common_scancode(Input::Keyboard key)
@@ -117,6 +119,10 @@ unsigned char sdl_key_from_common_scancode(Input::Keyboard key)
     case Input::Keyboard::K_NUM_SLASH: return SDL_SCANCODE_KP_DIVIDE;
     case Input::Keyboard::K_NUM_STAR: return SDL_SCANCODE_KP_MULTIPLY;
     case Input::Keyboard::K_NUM_ENTER: return SDL_SCANCODE_KP_EQUALS;
-    default: return SDL_SCANCODE_UNKNOWN;
+    case Input::Keyboard::K_NONUS_BACKSLASH: return SDL_SCANCODE_NONUSBACKSLASH;
+    case Input::Keyboard::K_NONUS_HASH: return SDL_SCANCODE_NONUSHASH;
+    case Input::Keyboard::K_COUNT: break;
     }
+    LVF_VERIFY(false && "key is SDL_SCANCODE_UNKNOWN");
+    return SDL_SCANCODE_UNKNOWN;
 }

commit 8975ddc8c2e2e204c5b74397b463b91a2c99baa9

fix: handle ISO key mappings properly
Fixes index-out-of-bounds-read on Windows.

NONUS_HASH is actually emitted as backslash, so there is no need for it.

Fixes https://github.com/chown2/lunaticvibesf/issues/133 "Support for
ISO keyboards".

Fixes: 1c5a5c247bd7 ("fix: interpret NONUS_BACKSLASH and NONUS_HASH")

src/common/keymap.h
@@ -228,7 +228,6 @@ enum class Keyboard : unsigned
 
     // ISO
     K_NONUS_BACKSLASH,
-    K_NONUS_HASH,
 
     // scancodes with mask (e0)
     K_NUM_SLASH,
@@ -373,7 +372,6 @@ inline const char* keyboardNameMap[keyboardKeyCount]{
 
     // ISO
     "NONUS_BACKSLASH",
-    "NONUS_HASH",
 };
 
 enum class JoystickAxis

src/game/graphics/SDL2/input.cpp
@@ -120,7 +120,6 @@ unsigned char sdl_key_from_common_scancode(Input::Keyboard key)
     case Input::Keyboard::K_NUM_STAR: return SDL_SCANCODE_KP_MULTIPLY;
     case Input::Keyboard::K_NUM_ENTER: return SDL_SCANCODE_KP_EQUALS;
     case Input::Keyboard::K_NONUS_BACKSLASH: return SDL_SCANCODE_NONUSBACKSLASH;
-    case Input::Keyboard::K_NONUS_HASH: return SDL_SCANCODE_NONUSHASH;
     case Input::Keyboard::K_COUNT: break;
     }
     LVF_VERIFY(false && "key is SDL_SCANCODE_UNKNOWN");

src/game/input/input_dinput8.h
@@ -9,6 +9,8 @@
 #include <dinput.h>
 #pragma comment(lib, "dinput8.lib")
 
+#include <vector>
+
 class InputDirectInput8
 {
 public:

src/game/input/input_windows.cpp
@@ -20,7 +20,6 @@ void pollInput()
 
 bool isKeyPressed(Input::Keyboard key)
 {
-    // these are mappings toward enum Input::Keyboard
     // refer to virtual key definitions in MSDN
     // https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
     static const int vkMap[] = {
@@ -146,10 +145,13 @@ bool isKeyPressed(Input::Keyboard key)
         DIK_CONVERT,
         DIK_KANA,
 
+        DIK_OEM_102, // NONUS_BACKSLASH
+
         DIK_NUMPADSLASH,
         DIK_NUMPADSTAR,
         DIK_NUMPADENTER,
     };
+    static_assert(std::size(vkMap) == static_cast<size_t>(Input::Keyboard::K_COUNT));
 
     int vk = vkMap[static_cast<size_t>(key)];
     return InputDirectInput8::inst().getKeyboardState()[vk] & 0x80;
@@ -172,13 +174,13 @@ bool isButtonPressed(Input::Joystick c, double deadzone)
                 float x = std::sinf(stat.rgdwPOV[c.index & 0xFFFFFFF] / 100.0f * 0.0174532925f);
                 float y = std::cosf(stat.rgdwPOV[c.index & 0xFFFFFFF] / 100.0f * 0.0174532925f);
 
-                if (x < -0.01f && (c.index & (1ul << 31)))
+                if (x < -0.01f && (c.index & (1ULL << 31)))
                     return true;
-                else if (y < -0.01f && (c.index & (1ul << 30)))
+                if (y < -0.01f && (c.index & (1ULL << 30)))
                     return true;
-                else if (y > 0.01f && (c.index & (1ul << 29)))
+                if (y > 0.01f && (c.index & (1ULL << 29)))
                     return true;
-                else if (x > 0.01f && (c.index & (1ul << 28)))
+                if (x > 0.01f && (c.index & (1ULL << 28)))
                     return true;
             }
             return false;
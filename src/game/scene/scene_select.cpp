MatVeiQaaa/lunaticvibesf

commit b38011a2dbb4b053fb492cd4652e3f82ff68a933

refactor: reduce copy-paste

src/game/scene/scene_select.cpp
@@ -2099,39 +2099,30 @@ void SceneSelect::decide()
         if (gChartContext.chart->type() == eChartFormat::BMS)
         {
             auto pBMS = std::reinterpret_pointer_cast<ChartFormatBMSMeta>(gChartContext.chart);
-            switch (pBMS->gamemode)
-            {
-            case 5:  gPlayContext.mode = SkinType::PLAY5;  break;
-            case 7:  gPlayContext.mode = SkinType::PLAY7;  break;
-            case 9:  gPlayContext.mode = SkinType::PLAY9;  break;
-            case 10: gPlayContext.mode = SkinType::PLAY10; break;
-            case 14: gPlayContext.mode = SkinType::PLAY14; break;
-            default: gPlayContext.mode = SkinType::PLAY7;  break;
-            }
+            gPlayContext.mode = lunaticvibes::skinTypeForKeys(pBMS->gamemode);
             if (gPlayContext.isBattle)
             {
                 if (State::get(IndexOption::PLAY_BATTLE_TYPE) == Option::BATTLE_LOCAL || 
                     State::get(IndexOption::PLAY_BATTLE_TYPE) == Option::BATTLE_GHOST)
                 {
-                    switch (pBMS->gamemode)
-                    {
-                    case 5:  gPlayContext.mode = SkinType::PLAY5_2;  break;
-                    case 7:  gPlayContext.mode = SkinType::PLAY7_2;  break;
-                    case 9:  gPlayContext.mode = SkinType::PLAY9;  gPlayContext.isBattle = false; break;
-                    case 10: gPlayContext.mode = SkinType::PLAY10; gPlayContext.isBattle = false; break;
-                    case 14: gPlayContext.mode = SkinType::PLAY14; gPlayContext.isBattle = false; break;
-                    default: assert(false); break;
-                    }
+                    gPlayContext.mode = lunaticvibes::skinTypeForKeys(pBMS->gamemode);
+                    auto canBattleInGameMode = [](unsigned keys) {
+                        switch (keys)
+                        {
+                        case 5:
+                        case 7: return true;
+                        case 9:
+                        case 10:
+                        case 14: return false;
+                        default: assert(false); return false;
+                        }
+                    };
+                    gPlayContext.isBattle = canBattleInGameMode(pBMS->gamemode);
                 }
             }
             else if (State::get(IndexOption::PLAY_BATTLE_TYPE) == Option::BATTLE_DB)
             {
-                switch (pBMS->gamemode)
-                {
-                case 5:  gPlayContext.mode = SkinType::PLAY10;  break;
-                case 7:  gPlayContext.mode = SkinType::PLAY14;  break;
-                default: assert(false); break;
-                }
+                gPlayContext.mode = lunaticvibes::skinTypeForKeys(pBMS->gamemode);
                 gChartContext.isDoubleBattle = true;
             }
         }

src/common/types.h
@@ -59,6 +59,38 @@ enum class SkinType : uint8_t {
 };
 std::ostream& operator<<(std::ostream& os, const SkinType& mode);
 
+namespace lunaticvibes {
+
+[[nodiscard]] inline SkinType skinTypeForKeys(unsigned keys) {
+    switch (keys)
+    {
+    case 5: return SkinType::PLAY5;
+    case 7: return SkinType::PLAY7;
+    case 9: return SkinType::PLAY9;
+    case 10: return SkinType::PLAY10;
+    case 14: return SkinType::PLAY14;
+    default: abort();
+    }
+}
+
+[[nodiscard]] inline unsigned skinTypeToKeys(SkinType mode)
+{
+    switch (mode)
+    {
+    case SkinType::PLAY5:
+    case SkinType::PLAY5_2: return 5;
+    case SkinType::PLAY7:
+    case SkinType::PLAY7_2: return 7;
+    case SkinType::PLAY9:
+    case SkinType::PLAY9_2: return 9;
+    case SkinType::PLAY10: return 10;
+    case SkinType::PLAY14: return 14;
+    default: abort();
+    }
+}
+
+} // namespace lunaticvibes
+
 enum class ePlayMode
 {
     SINGLE_PLAYER,  // means "Single Player Mode", so DP is also included

src/game/arena/arena_data.cpp
@@ -69,22 +69,8 @@ bool ArenaData::isPlayerReady(size_t index)
 
 void ArenaData::initPlaying(RulesetType rulesetType)
 {
-	unsigned keys = 7;
-	switch (gPlayContext.mode)
-	{
-	case SkinType::PLAY5:
-	case SkinType::PLAY5_2: keys = 5; break;
-	case SkinType::PLAY7:
-	case SkinType::PLAY7_2: keys = 7; break;
-	case SkinType::PLAY9:
-	case SkinType::PLAY9_2: keys = 9; break;
-	case SkinType::PLAY10: keys = 10; break;
-	case SkinType::PLAY14: keys = 14; break;
-	default: break;
-	}
-
+	const unsigned keys = lunaticvibes::skinTypeToKeys(gPlayContext.mode);
 	unsigned idx = 0;
-
 	for (auto& [id, d] : data)
 	{
 		switch (rulesetType)

src/game/game.cpp
@@ -6,6 +6,7 @@
 #include "game/scene/scene_mgr.h"
 #include "game/input/input_mgr.h"
 #include "game/skin/skin_lr2.h"
+#include "game/skin/skin_lr2_slider_callbacks.h"
 #include "game/skin/skin_mgr.h"
 #include "common/utils.h"
 #include "common/sysutil.h"
@@ -156,39 +157,13 @@ int main(int argc, char* argv[])
     SoundMgr::setVolume(SampleChannel::MASTER, (float)State::get(IndexSlider::VOLUME_MASTER));
     SoundMgr::setVolume(SampleChannel::KEY, (float)State::get(IndexSlider::VOLUME_KEY));
     SoundMgr::setVolume(SampleChannel::BGM, (float)State::get(IndexSlider::VOLUME_BGM));
-
     if (State::get(IndexSwitch::SOUND_FX0))
         SoundMgr::setDSP((DSPType)State::get(IndexOption::SOUND_FX0), 0, (SampleChannel)State::get(IndexOption::SOUND_TARGET_FX0), State::get(IndexSlider::FX0_P1), State::get(IndexSlider::FX0_P2));
     if (State::get(IndexSwitch::SOUND_FX1))
         SoundMgr::setDSP((DSPType)State::get(IndexOption::SOUND_FX1), 1, (SampleChannel)State::get(IndexOption::SOUND_TARGET_FX1), State::get(IndexSlider::FX1_P1), State::get(IndexSlider::FX1_P2));
     if (State::get(IndexSwitch::SOUND_FX2))
         SoundMgr::setDSP((DSPType)State::get(IndexOption::SOUND_FX2), 2, (SampleChannel)State::get(IndexOption::SOUND_TARGET_FX2), State::get(IndexSlider::FX2_P1), State::get(IndexSlider::FX2_P2));
-
-    if (State::get(IndexSwitch::SOUND_PITCH))
-    {
-        static const double tick = std::pow(2, 1.0 / 12);
-        double f = std::pow(tick, State::get(IndexNumber::PITCH));
-        switch (State::get(IndexOption::SOUND_PITCH_TYPE))
-        {
-        case 0: // FREQUENCY
-            SoundMgr::setFreqFactor(f);
-            gSelectContext.pitchSpeed = f;
-            break;
-        case 1: // PITCH
-            SoundMgr::setFreqFactor(1.0);
-            SoundMgr::setPitch(f);
-            gSelectContext.pitchSpeed = 1.0;
-            break;
-        case 2: // SPEED (freq up, pitch down)
-            SoundMgr::setFreqFactor(1.0);
-            SoundMgr::setSpeed(f);
-            gSelectContext.pitchSpeed = f;
-            break;
-        default:
-            break;
-        }
-    }
-
+    lr2skin::slider::updatePitchState(State::get(IndexNumber::PITCH));
     if (State::get(IndexSwitch::SOUND_EQ))
     {
         for (int idx = 0; idx < 7; ++idx)
@@ -217,18 +192,7 @@ int main(int argc, char* argv[])
         gQuitOnFinish = true;
 
         std::shared_ptr<ChartFormatBMS> bms = std::make_shared<ChartFormatBMS>(bmsFile, std::time(NULL));
-        auto skinTypeForKeys = [](unsigned keys) {
-            switch (keys)
-            {
-            case 5: return SkinType::PLAY5;
-            case 7: return SkinType::PLAY7;
-            case 9: return SkinType::PLAY9;
-            case 10: return SkinType::PLAY10;
-            case 14: return SkinType::PLAY14;
-            default: panic("Error", "Invalid chart key count");
-            }
-        };
-        gPlayContext.mode = skinTypeForKeys(bms->gamemode);
+        gPlayContext.mode = lunaticvibes::skinTypeForKeys(bms->gamemode);
         gChartContext = ChartContextParams{
             bmsFile,
             md5file(bmsFile),

src/game/scene/scene_mgr.cpp
@@ -1,6 +1,7 @@
 #include "scene_mgr.h"
 
 #include "common/log.h"
+#include "game/scene/scene.h"
 #include "scene_context.h"
 #include "scene_pre_select.h"
 #include "scene_select.h"
@@ -35,33 +36,21 @@ pScene SceneMgr::get(SceneType e)
     case SceneType::SELECT: ps = std::make_shared<SceneSelect>(); break;
     case SceneType::DECIDE: ps = std::make_shared<SceneDecide>(); break;
     case SceneType::PLAY:
-        switch (gPlayContext.mode)
+        if (getSceneFromSkinType(gPlayContext.mode) != SceneType::PLAY)
         {
-        case SkinType::PLAY5:
-        case SkinType::PLAY7:
-        case SkinType::PLAY9:
-        case SkinType::PLAY10:
-        case SkinType::PLAY14:
-        case SkinType::PLAY5_2:
-        case SkinType::PLAY7_2:
-        case SkinType::PLAY9_2: ps = std::make_shared<ScenePlay>(); break;
-        default: LOG_ERROR << "[Scene] Invalid mode: " << int(gPlayContext.mode); return nullptr;
+            LOG_ERROR << "[Scene] Invalid mode: " << int(gPlayContext.mode);
+            return nullptr;
         }
+        ps = std::make_shared<ScenePlay>();
         break;
     case SceneType::RETRY_TRANS: ps = std::make_shared<ScenePlayRetryTrans>(); break;
     case SceneType::RESULT:
-        switch (gPlayContext.mode)
+        if (getSceneFromSkinType(gPlayContext.mode) != SceneType::PLAY)
         {
-        case SkinType::PLAY5:
-        case SkinType::PLAY7:
-        case SkinType::PLAY9:
-        case SkinType::PLAY10:
-        case SkinType::PLAY14:
-        case SkinType::PLAY5_2:
-        case SkinType::PLAY7_2:
-        case SkinType::PLAY9_2: ps = std::make_shared<SceneResult>(); break;
-        default: LOG_ERROR << "[Scene] Invalid mode: " << int(gPlayContext.mode); return nullptr;
+            LOG_ERROR << "[Scene] Invalid mode: " << int(gPlayContext.mode);
+            return nullptr;
         }
+        ps = std::make_shared<SceneResult>();
         break;
     case SceneType::COURSE_TRANS: ps = std::make_shared<ScenePlayCourseTrans>(); break;
     case SceneType::KEYCONFIG: ps = std::make_shared<SceneKeyConfig>(); break;

src/game/scene/scene_play.cpp
@@ -160,15 +160,7 @@ ScenePlay::ScenePlay(): SceneBase(gPlayContext.mode, 1000, true)
     {
         gPlayContext.shift1PNotes5KFor7KSkin = (pSkin->info.scratchSide1P == 1);
         gPlayContext.shift2PNotes5KFor7KSkin = (pSkin->info.scratchSide2P == 1);
-
-        if (gPlayContext.shift1PNotes5KFor7KSkin)
-        {
-            replayCmdMapIndex = gPlayContext.shift2PNotes5KFor7KSkin ? 3 : 2;
-        }
-        else
-        {
-            replayCmdMapIndex = gPlayContext.shift2PNotes5KFor7KSkin ? 1 : 0;
-        }
+        replayCmdMapIndex = gPlayContext.shiftFiveKeyForSevenKeyIndex(true);
     }
 
     // ? 
@@ -796,35 +788,11 @@ bool ScenePlay::createRuleset()
             break;
         }
 
-        unsigned keys = 7;
-        switch (gPlayContext.mode)
-        {
-        case SkinType::PLAY5:
-        case SkinType::PLAY5_2: keys = 5; break;
-        case SkinType::PLAY7:
-        case SkinType::PLAY7_2: keys = 7; break;
-        case SkinType::PLAY9:
-        case SkinType::PLAY9_2: keys = 9; break;
-        case SkinType::PLAY10: keys = 10; break;
-        case SkinType::PLAY14: keys = 14; break;
-        default: break;
-        }
+        const unsigned keys = lunaticvibes::skinTypeToKeys(gPlayContext.mode);
 
         if (!gInCustomize)
         {
-            unsigned skinKeys = 7;
-            switch (pSkin->info.mode)
-            {
-            case SkinType::PLAY5:
-            case SkinType::PLAY5_2: skinKeys = 5; break;
-            case SkinType::PLAY7:
-            case SkinType::PLAY7_2: skinKeys = 7; break;
-            case SkinType::PLAY9:
-            case SkinType::PLAY9_2: skinKeys = 9; break;
-            case SkinType::PLAY10: skinKeys = 10; break;
-            case SkinType::PLAY14: skinKeys = 14; break;
-            default: break;
-            }
+            const unsigned skinKeys = lunaticvibes::skinTypeToKeys(pSkin->info.mode);
             InputMgr::updateBindings(skinKeys);
         }
 

src/game/skin/skin_lr2.cpp
@@ -35,6 +35,7 @@ static MotionKeyFrameParams::accelType parseAccelType(const int value)
     case 1: return accelType::ACCEL;
     case 2: return accelType::DECEL;
     case 3: return accelType::DISCONTINOUS;
+    default: break;
     }
     LOG_WARNING << "[SkinLR2] Invalid accelType '" << value << "'";
     return accelType::CONSTANT;
@@ -302,9 +303,9 @@ static int flipTimer(int timer)
         }
     };
 
-    typedef s_basic s_judgeline;
-    typedef s_basic s_barline;
-    typedef s_basic s_note;
+    using s_judgeline = s_basic;
+    using s_barline = s_basic;
+    using s_note = s_basic;
 
     struct s_nowjudge : s_basic
     {
@@ -352,10 +353,10 @@ static int flipTimer(int timer)
         }
     };
 
-    typedef s_basic s_barbody;
-    typedef s_basic s_barflash;
-    typedef s_nowcombo s_barlevel;
-    typedef s_basic s_barlamp;
+    using s_barbody = s_basic;
+    using s_barflash = s_basic;
+    using s_barlevel = s_nowcombo;
+    using s_barlamp = s_basic;
     struct s_bartitle
     {
         int _null = 0;
@@ -368,8 +369,8 @@ static int flipTimer(int timer)
             convertLine(tokens, (int*)this, 0, count);
         }
     };
-    typedef s_basic s_barrank;
-    typedef s_basic s_barrival;
+    using s_barrank = s_basic;
+    using s_barrival = s_basic;
     struct s_readme
     {
         int _null = 0;
@@ -382,7 +383,7 @@ static int flipTimer(int timer)
             convertLine(tokens, (int*)this, 0, count);
         }
     };
-    typedef s_basic s_mousecursor;
+    using s_mousecursor = s_basic;
 
     struct s_gaugechart : s_basic
     {
@@ -397,7 +398,7 @@ static int flipTimer(int timer)
             convertLine(tokens, (int*)this, offset, count);
         }
     };
-    typedef s_gaugechart s_scorechart;
+    using s_scorechart = s_gaugechart;
 
     struct dst
     {
@@ -432,21 +433,21 @@ static int flipTimer(int timer)
 
     BlendMode convertBlend(int blend)
     {
-        static const std::map<int, BlendMode> blendMap =
+        switch (blend)
         {
-            {0, BlendMode::NONE},
-            {1, BlendMode::ALPHA},
-            {2, BlendMode::ADD},
-            {3, BlendMode::SUBTRACT},
-            {4, BlendMode::MOD},
-            {6, BlendMode::ADD},    // LR2: XOR but implemented as ADD
-            {9, BlendMode::MULTIPLY_INVERTED_BACKGROUND},
-            {10, BlendMode::INVERT},
-            {11, BlendMode::MULTIPLY_WITH_ALPHA},
+        case 0: return BlendMode::NONE;
+        case 1: return BlendMode::ALPHA;
+        case 2: return BlendMode::ADD;
+        case 3: return BlendMode::SUBTRACT;
+        case 4: return BlendMode::MOD;
+        case 6: return BlendMode::ADD; // LR2: XOR but implemented as ADD
+        case 9: return BlendMode::MULTIPLY_INVERTED_BACKGROUND;
+        case 10: return BlendMode::INVERT;
+        case 11: return BlendMode::MULTIPLY_WITH_ALPHA;
+        default: return BlendMode::ALPHA;
         };
-        return (blendMap.find(blend) != blendMap.end()) ? blendMap.at(blend) : BlendMode::ALPHA;
     }
-}
+    } // namespace lr2skin
 
 std::map<std::string, Path> SkinLR2::LR2SkinFontPathCache;
 std::map<Path, std::shared_ptr<SkinLR2::LR2Font>> SkinLR2::LR2FontCache;
@@ -457,63 +458,40 @@ std::map<std::string, std::shared_ptr<SkinLR2::LR2Font>> SkinLR2::LR2FontNameMap
 int SkinLR2::setExtendedProperty(const std::string_view key, void* value)
 {
     assert(value != nullptr);
+
+    auto gaugeDisplayTypeToSpriteType = [](GaugeDisplayType type) -> SpriteGaugeGrid::GaugeType {
+        switch (type)
+        {
+        case GaugeDisplayType::GROOVE: return SpriteGaugeGrid::GaugeType::GROOVE;
+        case GaugeDisplayType::SURVIVAL: return SpriteGaugeGrid::GaugeType::SURVIVAL;
+        case GaugeDisplayType::EX_SURVIVAL: return SpriteGaugeGrid::GaugeType::EX_SURVIVAL;
+        case GaugeDisplayType::ASSIST_EASY: return SpriteGaugeGrid::GaugeType::ASSIST_EASY;
+        }
+        assert(false);
+        return {};
+    };
+
     if (key == "GAUGETYPE_1P")
     {
-        if (gSprites[GLOBAL_SPRITE_IDX_1PGAUGE] != nullptr)
+        if (gSprites[GLOBAL_SPRITE_IDX_1PGAUGE] == nullptr)
         {
-            auto type = *(GaugeDisplayType*)value;
-            auto pS = std::reinterpret_pointer_cast<SpriteGaugeGrid>(gSprites[GLOBAL_SPRITE_IDX_1PGAUGE]);
-            switch (type)
-            {
-            case GaugeDisplayType::SURVIVAL:
-                pS->setGaugeType(SpriteGaugeGrid::GaugeType::SURVIVAL);
-                break;
-
-            case GaugeDisplayType::EX_SURVIVAL:
-                pS->setGaugeType(SpriteGaugeGrid::GaugeType::EX_SURVIVAL);
-                break;
-
-            case GaugeDisplayType::ASSIST_EASY:
-                pS->setGaugeType(SpriteGaugeGrid::GaugeType::ASSIST_EASY);
-                break;
-
-            default:
-                pS->setGaugeType(SpriteGaugeGrid::GaugeType::GROOVE);
-                break;
-            }
-        }
-        else
             return 1;
+        }
+        auto type = *static_cast<GaugeDisplayType*>(value);
+        auto pS = std::reinterpret_pointer_cast<SpriteGaugeGrid>(gSprites[GLOBAL_SPRITE_IDX_1PGAUGE]);
+        pS->setGaugeType(gaugeDisplayTypeToSpriteType(type));
         return 0;
     }
 
     if (key == "GAUGETYPE_2P")
     {
-        if (gSprites[GLOBAL_SPRITE_IDX_2PGAUGE] != nullptr)
+        if (gSprites[GLOBAL_SPRITE_IDX_2PGAUGE] == nullptr)
         {
-            auto type = *(GaugeDisplayType*)value;
-            auto pS = std::reinterpret_pointer_cast<SpriteGaugeGrid>(gSprites[GLOBAL_SPRITE_IDX_2PGAUGE]);
-            switch (type)
-            {
-            case GaugeDisplayType::SURVIVAL:
-                pS->setGaugeType(SpriteGaugeGrid::GaugeType::SURVIVAL);
-                break;
-
-            case GaugeDisplayType::EX_SURVIVAL:
-                pS->setGaugeType(SpriteGaugeGrid::GaugeType::EX_SURVIVAL);
-                break;
-
-            case GaugeDisplayType::ASSIST_EASY:
-                pS->setGaugeType(SpriteGaugeGrid::GaugeType::ASSIST_EASY);
-                break;
-
-            default:
-                pS->setGaugeType(SpriteGaugeGrid::GaugeType::GROOVE);
-                break;
-            }
-        }
-        else
             return 1;
+        }
+        auto type = *static_cast<GaugeDisplayType*>(value);
+        auto pS = std::reinterpret_pointer_cast<SpriteGaugeGrid>(gSprites[GLOBAL_SPRITE_IDX_2PGAUGE]);
+        pS->setGaugeType(gaugeDisplayTypeToSpriteType(type));
         return 0;
     }
 
@@ -669,11 +647,8 @@ Path SkinLR2::getCustomizePath(StringContentView input)
         {
             return Path();
         }
-        else
-        {
-            size_t ranidx = std::rand() % ls.size();
-            return ls[ranidx];
-        }
+        size_t ranidx = std::rand() % ls.size();
+        return ls[ranidx];
     }
 
     // Normal path
@@ -1003,66 +978,57 @@ int SkinLR2::TIMEOPTION()
             if (update > 0) info.timeResultRecord = update;
             LOG_DEBUG << "[Skin] " << csvLineNumber << ": STARTINPUT " << info.timeIntro << " " << rank << " " << update;
         }
-
         return 1;
     }
-
-    else if (matchToken(parseKeyBuf, "#LOADSTART"))
+    if (matchToken(parseKeyBuf, "#LOADSTART"))
     {
         int time = toInt(parseParamBuf[0]);
         info.timeStartLoading = time;
         LOG_DEBUG << "[Skin] " << csvLineNumber << ": Set time colddown before loading: " << time;
         return 3;
     }
-
-    else if (matchToken(parseKeyBuf, "#LOADEND"))
+    if (matchToken(parseKeyBuf, "#LOADEND"))
     {
         int time = toInt(parseParamBuf[0]);
         info.timeMinimumLoad = time;
         LOG_DEBUG << "[Skin] " << csvLineNumber << ": Set time colddown after loading: " << time;
         return 4;
     }
-
-    else if (matchToken(parseKeyBuf, "#PLAYSTART"))
+    if (matchToken(parseKeyBuf, "#PLAYSTART"))
     {
         int time = toInt(parseParamBuf[0]);
         info.timeGetReady = time;
         LOG_DEBUG << "[Skin] " << csvLineNumber << ": Set time READY after loading: " << time;
         return 5;
     }
-
-    else if (matchToken(parseKeyBuf, "#CLOSE"))
+    if (matchToken(parseKeyBuf, "#CLOSE"))
     {
         int time = toInt(parseParamBuf[0]);
         info.timeFailed = time;
         LOG_DEBUG << "[Skin] " << csvLineNumber << ": Set FAILED time length: " << time;
         return 6;
     }
-
-    else if (matchToken(parseKeyBuf, "#FADEOUT"))
+    if (matchToken(parseKeyBuf, "#FADEOUT"))
     {
         int time = toInt(parseParamBuf[0]);
         info.timeOutro = time;
         LOG_DEBUG << "[Skin] " << csvLineNumber << ": Set fadeout time length: " << time;
         return 7;
     }
-
-    else if (matchToken(parseKeyBuf, "#SKIP"))
+    if (matchToken(parseKeyBuf, "#SKIP"))
     {
         int time = toInt(parseParamBuf[0]);
         info.timeDecideSkip = time;
         LOG_DEBUG << "[Skin] " << csvLineNumber << ": Set Decide skip time: " << time;
         return 8;
     }
-
-    else if (matchToken(parseKeyBuf, "#SCENETIME"))
+    if (matchToken(parseKeyBuf, "#SCENETIME"))
     {
         int time = toInt(parseParamBuf[0]);
         info.timeDecideExpiry = time;
         LOG_DEBUG << "[Skin] " << csvLineNumber << ": Set Decide expiry time: " << time;
         return 9;
     }
-
     return 0;
 }
 
@@ -3666,16 +3632,7 @@ SkinLR2::~SkinLR2()
 {
     stopSpriteVideoPlayback();
 
-    switch (info.mode)
-    {
-    case SkinType::PLAY5:
-    case SkinType::PLAY5_2:
-    case SkinType::PLAY7:
-    case SkinType::PLAY7_2:
-    case SkinType::PLAY9:
-    case SkinType::PLAY9_2:
-    case SkinType::PLAY10:
-    case SkinType::PLAY14:
+    if (getSceneFromSkinType(info.mode) == SceneType::PLAY)
     {
         Path pCustomize = ConfigMgr::Profile()->getPath() / "customize" / SceneCustomize::getConfigFileName(getFilePath());
         try
@@ -3708,9 +3665,6 @@ SkinLR2::~SkinLR2()
         {
             LOG_WARNING << "[Skin] Bad customize config file: " << pCustomize;
         }
-        break;
-    }
-    default: break;
     }
 }
 
@@ -4409,16 +4363,20 @@ void SkinLR2::update()
         move1PY -= lift1P;
         move2PY -= lift2P;
     }
-    if (move1PX != 0 || move1PY != 0)
-    {
-        for (size_t i = 0; i < 6; ++i)
+    auto adjustPlayJudgePosition = [](size_t base, int movex, int movey) {
+        if (movex == 0 && movey == 0)
+        {
+            return;
+        }
+
+        for (size_t i = base; i < base + 6; ++i)
         {
             if (gSprites[i])
             {
                 std::shared_ptr<SpriteAnimated> judge = std::reinterpret_pointer_cast<SpriteAnimated>(gSprites[i]);
                 if (judge->isDraw() && !judge->isHidden())
                 {
-                    judge->adjustAfterUpdate(move1PX, move1PY);
+                    judge->adjustAfterUpdate(movex, movey);
                 }
             }
             if (gSprites[i + 6])
@@ -4426,61 +4384,38 @@ void SkinLR2::update()
                 std::shared_ptr<SpriteNumber> combo = std::reinterpret_pointer_cast<SpriteNumber>(gSprites[i + 6]);
                 if (combo->isDraw() && !combo->isHidden())
                 {
-                    combo->adjustAfterUpdate(move1PX, move1PY);
+                    combo->adjustAfterUpdate(movex, movey);
                 }
             }
         }
-    }
-    if (move2PX != 0 || move2PY != 0)
-    {
-        for (size_t i = 0; i < 6; ++i)
-        {
-            if (gSprites[i + 12])
-            {
-                std::shared_ptr<SpriteAnimated> judge = std::reinterpret_pointer_cast<SpriteAnimated>(gSprites[i + 12]);
-                if (judge->isDraw() && !judge->isHidden())
-                {
-                    judge->adjustAfterUpdate(move2PX, move2PY);
-                }
-            }
-            if (gSprites[i + 18])
-            {
-                std::shared_ptr<SpriteNumber> combo = std::reinterpret_pointer_cast<SpriteNumber>(gSprites[i + 18]);
-                if (combo->isDraw() && !combo->isHidden())
-                {
-                    combo->adjustAfterUpdate(move2PX, move2PY);
-                }
-            }
-        }
-    }
+    };
+    adjustPlayJudgePosition(0, move1PX, move1PY);
+    adjustPlayJudgePosition(12, move2PX, move2PY);
 
     // LIFT: judgeline, sprites
     move1PX = adjustPlayNote1PX;
     move1PY = adjustPlayNote1PY - lift1P;
     move2PX = adjustPlayNote2PX;
     move2PY = adjustPlayNote2PY - lift2P;
-    if (move1PX != 0 || move1PY != 0)
-    {
-        std::shared_ptr<SpriteAnimated> judgeLine = std::reinterpret_pointer_cast<SpriteAnimated>(gSprites[GLOBAL_SPRITE_IDX_1PJUDGELINE]);
+    auto adjustLift = [](size_t judgeline_sprite, std::list<std::shared_ptr<SpriteBase>>& spritesMoveWithLift,
+                         int movex, int movey) {
+        if (movex == 0 && movey == 0)
+        {
+            return;
+        }
+        auto& judgeLine = gSprites[judgeline_sprite];
         if (judgeLine && judgeLine->isDraw() && !judgeLine->isHidden())
         {
-            judgeLine->adjustAfterUpdate(move1PX, move1PY);
+            judgeLine->adjustAfterUpdate(movex, movey);
         }
 
-        for (auto& s : spritesMoveWithLift1P)
-            s->adjustAfterUpdate(move1PX, move1PY);
-    }
-    if (move2PX != 0 || move2PY != 0)
-    {
-        std::shared_ptr<SpriteAnimated> judgeLine = std::reinterpret_pointer_cast<SpriteAnimated>(gSprites[GLOBAL_SPRITE_IDX_2PJUDGELINE]);
-        if (judgeLine && judgeLine->isDraw() && !judgeLine->isHidden())
+        for (auto& s : spritesMoveWithLift)
         {
-            judgeLine->adjustAfterUpdate(move2PX, move2PY);
+            s->adjustAfterUpdate(movex, movey);
         }
-
-        for (auto& s : spritesMoveWithLift2P)
-            s->adjustAfterUpdate(move2PX, move2PY);
-    }
+    };
+    adjustLift(GLOBAL_SPRITE_IDX_1PJUDGELINE, spritesMoveWithLift1P, move1PX, move1PY);
+    adjustLift(GLOBAL_SPRITE_IDX_2PJUDGELINE, spritesMoveWithLift2P, move2PX, move2PY);
 
     // note scale
     if (move1PX != 0 || move1PY != 0 || move2PX != 0 || move2PY != 0 || adjustPlayNote1PW != 0 ||
@@ -4611,8 +4546,9 @@ SkinBase::CustomizeOption SkinLR2::getCustomizeOptionInfo(size_t idx) const
         ret.internalName = "FILE_";
         ret.internalName += op.title;
         ret.displayName = op.title;
-        for (size_t i = 0; i < op.pathList.size(); ++i)
-            ret.entries.push_back(op.pathList[i].filename().stem().u8string());
+        ret.entries.reserve(op.pathList.size());
+        for (const auto& path : op.pathList)
+            ret.entries.push_back(path.filename().stem().u8string());
         ret.defaultEntry = op.defIdx;
         break;
     }

src/game/skin/skin_lr2_slider_callbacks.cpp
@@ -156,13 +156,8 @@ void fx2(int idx, double p)
     }
 }
 
-void pitch(double p)
+void updatePitchState(int val)
 {
-    int val = int(p * 24) - 12;
-    double ps = (val + 12) / 24.0;
-    State::set(IndexSlider::PITCH, ps);
-    State::set(IndexNumber::PITCH, val);
-
     if (State::get(IndexSwitch::SOUND_PITCH))
     {
         static const double tick = std::pow(2, 1.0 / 12);
@@ -187,6 +182,16 @@ void pitch(double p)
             break;
         }
     }
+}
+
+void pitch(double p)
+{
+    int val = int(p * 24) - 12;
+    double ps = (val + 12) / 24.0;
+    State::set(IndexSlider::PITCH, ps);
+    State::set(IndexNumber::PITCH, val);
+
+    updatePitchState(val);
 
     const auto [score, lamp] = getSaveScoreType();
     State::set(IndexSwitch::CHART_CAN_SAVE_SCORE, score);

commit c467c7ca8b021d50f36b292a3f0a20102e25c4dd

fix(game): properly select skin for battle modes
Ref https://github.com/chown2/lunaticvibesf/issues/138 "Battle modes not
working".

2P graph seems to be empty.
Ghost battle dies: `scene_play.cpp:2079: runtime error: member access
within null pointer of type 'struct Commands'`.

Fixes: b38011a2dbb4 ("refactor: reduce copy-paste")

src/game/scene/scene_select.cpp
@@ -2036,6 +2036,19 @@ static std::pair<std::shared_ptr<ChartFormatBase>, size_t> selectRandom(
     return {nullptr, 0};
 }
 
+[[nodiscard]] inline SkinType skinTypeForKeysBattle(unsigned keys)
+{
+    switch (keys)
+    {
+    case 5: return SkinType::PLAY5_2;
+    case 7: return SkinType::PLAY7_2;
+    case 9: return SkinType::PLAY9;
+    case 10: return SkinType::PLAY10;
+    case 14: return SkinType::PLAY14;
+    }
+    lunaticvibes::assert_failed("skinTypeForKeysBattle");
+}
+
 void SceneSelect::decide()
 {
     std::shared_lock<std::shared_mutex> u(gSelectContext._mutex);
@@ -2179,7 +2192,7 @@ void SceneSelect::decide()
                 if (State::get(IndexOption::PLAY_BATTLE_TYPE) == Option::BATTLE_LOCAL ||
                     State::get(IndexOption::PLAY_BATTLE_TYPE) == Option::BATTLE_GHOST)
                 {
-                    gPlayContext.mode = lunaticvibes::skinTypeForKeys(pBMS->gamemode);
+                    gPlayContext.mode = skinTypeForKeysBattle(pBMS->gamemode);
                     auto canBattleInGameMode = [](unsigned keys) {
                         switch (keys)
                         {
@@ -2209,17 +2222,14 @@ void SceneSelect::decide()
             LVF_DEBUG_ASSERT(!gPlayContext.isBattle);
             LVF_DEBUG_ASSERT(!gChartContext.isDoubleBattle);
             break;
-
         case SkinType::PLAY5_2:
         case SkinType::PLAY7_2:
         case SkinType::PLAY9_2:
             LVF_DEBUG_ASSERT(gPlayContext.isBattle);
             LVF_DEBUG_ASSERT(!gChartContext.isDoubleBattle);
             break;
-
         case SkinType::PLAY10:
         case SkinType::PLAY14: LVF_DEBUG_ASSERT(!gPlayContext.isBattle); break;
-
         default: break;
         }
 
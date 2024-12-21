8e059e40f487588068c6634d90947d10d5ba683a
chown2 <kuso@cock.email>
2024-12-21 01:03:10 +0000

    style: format CMake files with gersemi

--- src/game/git_version.cmake
+++ src/game/git_version.cmake
@@ -1,33 +1,38 @@
-
 find_package(Git)
 if(GIT_EXECUTABLE)
-    execute_process(COMMAND ${GIT_EXECUTABLE} describe --always --long --dirty
-        OUTPUT_VARIABLE GIT_REVISION)
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} describe --always --long --dirty
+        OUTPUT_VARIABLE GIT_REVISION
+    )
 
     string(STRIP "${GIT_REVISION}" GIT_REVISION)
 endif()
 
-set (GIT_VERSION_HEADER "#pragma once
+set(GIT_VERSION_HEADER
+    "#pragma once
 extern const char* GIT_REVISION\;
-")
-set (GIT_VERSION "#include \"git_version.h\"
+"
+)
+set(GIT_VERSION
+    "#include \"git_version.h\"
 const char* GIT_REVISION = \"${GIT_REVISION}\"\;
-")
+"
+)
 
-if (EXISTS "git_version.h")
+if(EXISTS "git_version.h")
     file(STRINGS "git_version.h" GIT_VERSION_HEADER_ NEWLINE_CONSUME)
 else()
     set(GIT_VERSION_HEADER_ "")
 endif()
-if (NOT "${GIT_VERSION_HEADER}" STREQUAL "${GIT_VERSION_HEADER_}")
+if(NOT "${GIT_VERSION_HEADER}" STREQUAL "${GIT_VERSION_HEADER_}")
     file(WRITE "git_version.h" ${GIT_VERSION_HEADER})
 endif()
 
-if (EXISTS "git_version.cpp")
+if(EXISTS "git_version.cpp")
     file(STRINGS "git_version.cpp" GIT_VERSION_ NEWLINE_CONSUME)
 else()
     set(GIT_VERSION_ "")
 endif()
-if (NOT "${GIT_VERSION}" STREQUAL "${GIT_VERSION_}")
+if(NOT "${GIT_VERSION}" STREQUAL "${GIT_VERSION_}")
     file(WRITE "git_version.cpp" ${GIT_VERSION})
 endif()

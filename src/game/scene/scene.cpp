MatVeiQaaa/lunaticvibesf

commit 126b2bdb59bf072637e8690dba98ca19036f0e89

refactor: fix clang-tidy warnings

src/game/scene/scene.cpp
@@ -1,5 +1,6 @@
 #include "scene.h"
 
+#include <cstddef>
 #include <functional>
 #include <memory>
 
@@ -158,7 +159,7 @@ void SceneBase::update()
 
             // notifications expire check
             while (!gOverlayContext.notifications.empty() &&
-                   (t - gOverlayContext.notifications.begin()->first).norm() > 10 * 1000) // 10s
+                   (t - gOverlayContext.notifications.begin()->first).norm() > static_cast<decltype(std::declval<timeNormRes>().count())>(10 * 1000)) // 10s
             {
                 gOverlayContext.notifications.pop_front();
             }

src/common/difficultytable/table_bms.cpp
@@ -16,7 +16,7 @@
 #include <common/types.h>
 #include <common/utils.h>
 
-enum class GetResult
+enum class GetResult : uint8_t
 {
     OK,
     ERR_UNKNOWN,
@@ -382,11 +382,9 @@ void DifficultyTableBMS::parseHeader(const std::string& content)
     try
     {
         std::string_view bodyview = content;
-        if (bodyview.substr(0, 3) == "\xef\xbb\xbf")
-        {
-            // Some tables start with a BOM, which needs to be removed
+        // Some tables start with a BOM, which needs to be removed
+        if (bodyview.starts_with("\xef\xbb\xbf"))
             bodyview = bodyview.substr(3);
-        }
 
         tao::json::value header = tao::json::from_string(bodyview);
         try

src/common/entry/entry_random_song.cpp
@@ -6,15 +6,15 @@
 #include "common/entry/entry.h"
 #include "common/hash.h"
 
-lunaticvibes::EntryRandomChart::EntryRandomChart(std::string name, std::string name2, Filter filter) : _filter(filter)
+lunaticvibes::EntryRandomChart::EntryRandomChart(std::string name_, std::string name2_, Filter filter) : _filter(filter)
 {
     _type = eEntryType::RANDOM_CHART;
-    _name = std::move(name);
-    _name2 = std::move(name2);
+    _name = std::move(name_);
+    _name2 = std::move(name2_);
 
     std::stringstream ss;
     ss << static_cast<int>(filter);
-    ss << name;
-    ss << name2;
+    ss << _name;
+    ss << _name2;
     this->md5 = ::md5(ss.str());
 };

src/common/utils.cpp
@@ -207,8 +207,8 @@ static HashMD5 format_hash(std::span<unsigned char> digest)
     {
         unsigned char high = digest[i] >> 4 & 0xF;
         unsigned char low = digest[i] & 0xF;
-        ret += (high <= 9 ? ('0' + high) : ('A' - 10 + high));
-        ret += (low <= 9 ? ('0' + low) : ('A' - 10 + low));
+        ret += static_cast<char>(high <= 9 ? ('0' + high) : ('A' - 10 + high));
+        ret += static_cast<char>(low <= 9 ? ('0' + low) : ('A' - 10 + low));
     }
     return HashMD5{ret};
 }

src/db/db_song.cpp
@@ -1145,13 +1145,13 @@ std::shared_ptr<EntryFolderRegular> SongDB::browse(const HashMD5& root, bool rec
 
     std::shared_ptr<EntryFolderRegular> list = std::make_shared<EntryFolderRegular>(root, path);
 
-    if (folderQueryParentMap.find(root) != folderQueryParentMap.end())
+    if (auto it = folderQueryParentMap.find(root); it != folderQueryParentMap.end())
     {
-        for (const auto& index : folderQueryParentMap.at(root))
+        for (const auto& index : it->second)
         {
             const auto& c = folderQueryPool[index];
             HashMD5 md5{ANY_STR(c[0])};
-            auto parent = ANY_STR(c[1]);
+            // auto parent = ANY_STR(c[1]);
             auto name = ANY_STR(c[2]);
             auto type = (FolderType)ANY_INT(c[3]);
             auto path = ANY_STR(c[4]);

src/game/graphics/dxa.cpp
@@ -1,5 +1,6 @@
 #include "dxa.h"
 
+#include <cstddef>
 #include <cstdio>
 #include <cstring>
 #include <filesystem>
@@ -122,7 +123,7 @@ typedef struct tagDARC_DIRECTORY_VER5
 // ファイル名データから元のファイル名の文字列を取得する
 const char* GetOriginalFileName(u8* FileNameTable)
 {
-    return (char*)FileNameTable + *((u16*)&FileNameTable[0]) * 4 + 4;
+    return (char*)FileNameTable + static_cast<ptrdiff_t>(*((u16*)&FileNameTable[0]) * 4) + 4;
 }
 
 // 鍵文字列を作成

src/game/graphics/video.cpp
@@ -84,7 +84,8 @@ int sVideo::setVideo(const Path& file, double speed, bool loop)
         return -3;
     }
 
-    if ((pCodec = avcodec_find_decoder(pFormatCtx->streams[videoIndex]->codecpar->codec_id)) == NULL)
+    pCodec = avcodec_find_decoder(pFormatCtx->streams[videoIndex]->codecpar->codec_id);
+    if (pCodec == nullptr)
     {
         auto id = pFormatCtx->streams[videoIndex]->codecpar->codec_id;
         auto desc = avcodec_descriptor_get(id);
@@ -96,7 +97,8 @@ int sVideo::setVideo(const Path& file, double speed, bool loop)
         return -3;
     }
 
-    if ((pCodecCtx = avcodec_alloc_context3(pCodec)) == nullptr)
+    pCodecCtx = avcodec_alloc_context3(pCodec);
+    if (pCodecCtx == nullptr)
     {
         LOG_WARNING << "[Video] Could not alloc codec context of " << file;
         return -4;

src/game/scene/scene.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include "common/types.h"
-#include "game/input/input_wrapper.h"
-#include "game/runtime/state.h"
-#include "game/skin/skin.h"
-#include "game/skin/skin_mgr.h"
+#include <common/types.h>
+#include <game/input/input_wrapper.h>
+#include <game/skin/skin.h>
+#include <game/skin/skin_mgr.h>
+
 #include <array>
 #include <atomic>
 #include <memory>

commit f8095afc3a2e23c379f626e00fabc4853c594b66

style: format code base
Fixes: 126b2bdb59bf ("refactor: fix clang-tidy warnings")

src/game/scene/scene.cpp
@@ -159,7 +159,8 @@ void SceneBase::update()
 
             // notifications expire check
             while (!gOverlayContext.notifications.empty() &&
-                   (t - gOverlayContext.notifications.begin()->first).norm() > static_cast<decltype(std::declval<timeNormRes>().count())>(10 * 1000)) // 10s
+                   (t - gOverlayContext.notifications.begin()->first).norm() >
+                       static_cast<decltype(std::declval<timeNormRes>().count())>(10 * 1000)) // 10s
             {
                 gOverlayContext.notifications.pop_front();
             }
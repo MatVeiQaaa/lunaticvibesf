MatVeiQaaa/lunaticvibesf

commit 6f2f60d8c4dc4b2875e05e17eab96d9cb482b4e7

fix(game): only allow turntable spin if not auto play and after started
Fixes https://github.com/chown2/lunaticvibesf/issues/109 (Can spin
turntable in auto and replays).

src/game/scene/scene_play.cpp
@@ -3310,11 +3310,14 @@ void ScenePlay::inputGameHold(InputMask& m, const lunaticvibes::Time& t)
             playerState[slot].hispeedAddPending += val;
         }
     };
-    ttUpdateSide(PLAYER_SLOT_PLAYER, S1L, S1R);
-    if (isPlaymodeDP())
-        ttUpdateSide(PLAYER_SLOT_PLAYER, S2L, S2R);
-    else if (gPlayContext.isBattle)
-        ttUpdateSide(PLAYER_SLOT_TARGET, S2L, S2R);
+    if (!gPlayContext.isAuto && !gPlayContext.isReplay && gChartContext.started)
+    {
+        ttUpdateSide(PLAYER_SLOT_PLAYER, S1L, S1R);
+        if (isPlaymodeDP())
+            ttUpdateSide(PLAYER_SLOT_PLAYER, S2L, S2R);
+        else if (gPlayContext.isBattle)
+            ttUpdateSide(PLAYER_SLOT_TARGET, S2L, S2R);
+    }
 }
 
 // CALLBACK

commit 0c7ef71d772185670c6c1c9f03e42f263061aad0

fix(game): allow turntable adjust before play started
https://github.com/chown2/lunaticvibesf/issues/109 "Can spin turntable
in auto and replays".

Fixes: 6f2f60d8c4dc ("fix(game): only allow turntable spin if not auto play and after started")

src/game/scene/scene_play.cpp
@@ -3407,7 +3407,7 @@ void ScenePlay::inputGameHold(InputMask& m, const lunaticvibes::Time& t)
             playerState[slot].hispeedAddPending += val;
         }
     };
-    if (!gPlayContext.isAuto && !gPlayContext.isReplay && gChartContext.started)
+    if (!gPlayContext.isAuto && !gPlayContext.isReplay)
     {
         ttUpdateSide(PLAYER_SLOT_PLAYER, S1L, S1R);
         if (isPlaymodeDP())
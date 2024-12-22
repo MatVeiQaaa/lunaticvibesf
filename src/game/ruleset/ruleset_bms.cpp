MatVeiQaaa/lunaticvibesf

commit db20d5c4873d16181ffe37c17753f28ff92eb2cc

feat: added autoadjust

src/game/ruleset/ruleset_bms.cpp
@@ -9,6 +9,7 @@
 #include "game/scene/scene_context.h"
 #include "game/sound/sound_mgr.h"
 #include "game/sound/sound_sample.h"
+#include <config/config_mgr.h>
 #include <common/assert.h>
 #include <common/sysutil.h>
 
@@ -834,6 +835,8 @@ void RulesetBMS::_judgePress(NoteLaneCategory cat, NoteLaneIndex idx, HitableNot
         }
     }
 }
+
+// Judges LNs in the case of overhold.
 void RulesetBMS::_judgeHold(NoteLaneCategory cat, NoteLaneIndex idx, HitableNote& note, const JudgeRes& judge,
                             const lunaticvibes::Time& t, int slot)
 {
@@ -1181,6 +1184,27 @@ void RulesetBMS::updateJudge(const lunaticvibes::Time& t, const NoteLaneIndex ch
     }
 }
 
+void RulesetBMS::updateAutoadjust(const HitableNote& pNote, const lunaticvibes::Time& rt)
+{
+    if (!State::get(IndexOption::PLAY_AUTOADJUST)) {
+        return;
+    }
+    bool isOffsetPositive = (rt - pNote.time).norm() >= 0; 
+    lunaticvibes::Time hitOffset = rt > pNote.time ? rt - pNote.time : pNote.time - rt;
+    if (hitOffset <= judgeTime[(int)_judgeDifficulty].BAD) {
+        _notesSinceLastAutoadjust++;
+        if (_notesSinceLastAutoadjust > 9)
+        {
+            if (hitOffset.norm() != 0)
+            {
+                int newAdjust = State::get(IndexNumber::TIMING_ADJUST_VISUAL) + (isOffsetPositive ? 1 : -1);
+                State::set(IndexNumber::TIMING_ADJUST_VISUAL, newAdjust );
+            }
+            _notesSinceLastAutoadjust = 0;
+        }
+    }
+}
+
 void RulesetBMS::judgeNotePress(const Input::Pad k, const lunaticvibes::Time& t, const lunaticvibes::Time& rt,
                                 const int slot)
 {
@@ -1209,11 +1233,13 @@ void RulesetBMS::judgeNotePress(const Input::Pad k, const lunaticvibes::Time& t,
     if (pNote1 && (pNote2 == nullptr || pNote1->time < pNote2->time) && !pNote1->expired)
     {
         j = _calcJudgeByTimes(*pNote1, rt);
+        updateAutoadjust(*pNote1, rt);
         _judgePress(NoteLaneCategory::Note, idx1, *pNote1, j, t, slot);
     }
     else if (pNote2 && !pNote2->expired)
     {
         j = _calcJudgeByTimes(*pNote2, rt);
+        updateAutoadjust(*pNote2, rt);
         _judgePress(NoteLaneCategory::LN, idx2, *pNote2, j, t, slot);
     }
 }
@@ -1247,7 +1273,6 @@ void RulesetBMS::judgeNoteRelease(Input::Pad k, const lunaticvibes::Time& t, con
         {
             if (!itNote->expired)
             {
-                ;
                 auto j = _calcJudgeByTimes(*itNote, rt);
                 _judgeRelease(NoteLaneCategory::LN, idx, *itNote, j, t, slot);
                 break;

src/game/ruleset/ruleset_bms.h
@@ -181,6 +181,9 @@ protected:
     std::string modifierText, modifierTextShort;
     Option::e_lamp_type saveLampMax;
 
+private:
+    unsigned int _notesSinceLastAutoadjust = 0;
+
 protected:
     // members change in game
     std::array<JudgeArea, chart::NOTELANEINDEX_COUNT> _lnJudge{JudgeArea::NOTHING};
@@ -206,10 +209,11 @@ public:
 
     void initGaugeParams(PlayModifierGaugeType gauge);
 
-protected:
+private:
     JudgeRes _calcJudgeByTimes(const Note& note, const lunaticvibes::Time& time) const;
 
-private:
+    void updateAutoadjust(const HitableNote& pNote, const lunaticvibes::Time& rt);
+
     void _judgePress(chart::NoteLaneCategory cat, chart::NoteLaneIndex idx, HitableNote& note, const JudgeRes& judge,
                      const lunaticvibes::Time& t, int slot);
     void _judgeHold(chart::NoteLaneCategory cat, chart::NoteLaneIndex idx, HitableNote& note, const JudgeRes& judge,

src/config/cfg_profile.h
@@ -89,6 +89,8 @@ enum class eTargetType
     IR_AVERAGE,
 };
 
+constexpr char P_JUDGE_AUTOADJUST[] = "uAutoAdjust";
+
 constexpr char P_CHART_OP[] = "ChartOption";
 constexpr char P_CHART_OP_2P[] = "ChartOption2P";
 constexpr char P_CHART_OP_NORMAL[] = "Normal";

src/config/config_mgr.cpp
@@ -132,6 +132,11 @@ void setOptions()
         }
     }
 
+    // autoadjust
+    {
+        State::set(IndexOption::PLAY_AUTOADJUST, ConfigMgr::get('P', P_JUDGE_AUTOADJUST, 0));
+    }
+
     // chart op
     {
         static const std::map<string, e_random_type> smap = {
@@ -780,9 +785,14 @@ void setText()
             State::set(IndexText::VSYNC, Option::s_vsync_mode[Option::VSYNC_OFF]);
     }
 
+    //autoadjust
+    {
+        State::set(IndexText::AUTOADJUST,
+                   Option::s_autoadjust[std::min((unsigned int)std::size(Option::s_autoadjust), (unsigned int)ConfigMgr::get('P', P_JUDGE_AUTOADJUST, 0))]);
+    }
+
     // fixed tokens
     State::set(IndexText::COLOR_DEPTH, "WHAT DO YOU WANT FROM THIS");
-    State::set(IndexText::JUDGE_AUTO, "NOT SUPPORTED");
     State::set(IndexText::REPLAY_SAVE_TYPE, "BEST EXSCORE");
 
     State::set(IndexText::TRIAL1, "NOT SUPPORTED");

src/game/runtime/index/option.h
@@ -54,6 +54,7 @@ enum class IndexOption : unsigned
     PLAY_HSFIX_TYPE,          // OFF/MAXBPM/MINBPM/AVERAGE/CONSTANT/START(new)/MAIN(new)
     PLAY_BATTLE_TYPE,         // OFF/BATTLE/DB/SPtoDP,9to7/GhostBattle
     PLAY_TARGET_TYPE,         // 0%/MYBEST/AAA/AA/A/DEFAULT/IRTOP/IRNEXT/IRAVERAGE
+    PLAY_AUTOADJUST,          // OFF/ON/SILENT(not supported)
 
     PLAY_RANK_ESTIMATED_1P,
     PLAY_RANK_ESTIMATED_2P,
@@ -287,6 +288,8 @@ enum e_target_type
 static constexpr const char* s_target_type[] = {
     "NO TARGET", "MY BEST", "RANK AAA", "RANK AA", "RANK A", "50%", "IR TOP", "IR NEXT", "IR AVERAGE",
 };
+
+static constexpr const char* s_autoadjust[] = { "OFF", "ON", "SILENT" };
 
 enum e_gauge_type
 {

src/game/runtime/index/text.h
@@ -69,7 +69,7 @@ enum class IndexText : unsigned
     COLOR_DEPTH,
     VSYNC,
     WINDOWMODE, // Fullscreen, Windowed
-    JUDGE_AUTO,
+    AUTOADJUST,
     REPLAY_SAVE_TYPE,
     TRIAL1,
     TRIAL2,

src/game/scene/scene_play.cpp
@@ -2493,7 +2493,7 @@ void ScenePlay::updateFadeout()
             State::set(IndexSlider::HISPEED_2P, gPlayContext.playerState[PLAYER_SLOT_TARGET].hispeed / 10.0);
         }
 
-        // save lanecover settings
+        // save play settings
         if (!gPlayContext.isReplay)
         {
             auto saveLanecoverSide = [&](int slot) {
@@ -2559,6 +2559,8 @@ void ScenePlay::updateFadeout()
             saveLanecoverSide(PLAYER_SLOT_PLAYER);
             if (gPlayContext.isBattle)
                 saveLanecoverSide(PLAYER_SLOT_TARGET);
+
+            ConfigMgr::set('P', cfg::P_JUDGE_OFFSET, State::get(IndexNumber::TIMING_ADJUST_VISUAL));
         }
 
         // reset BGA

src/game/skin/skin_lr2_button_callbacks.cpp
@@ -1000,8 +1000,14 @@ void bga_size(int plus)
 // 75
 void judge_auto_adjust(int plus)
 {
+    int val = (State::get(IndexOption::PLAY_AUTOADJUST) + 2 + plus) % 2;
+
+    State::set(IndexOption::PLAY_AUTOADJUST, val);
+    State::set(IndexText::AUTOADJUST, Option::s_autoadjust[val]);
 
     SoundMgr::playSysSample(SoundChannelType::KEY_SYS, eSoundSample::SOUND_O_CHANGE);
+
+    ConfigMgr::set('P', cfg::P_JUDGE_AUTOADJUST, State::get(IndexOption::PLAY_AUTOADJUST));
 }
 
 // 76

commit 9812a362b39ff08ced3d9c8db9e9da691c088afc

fix(game): fixup autoadjust
- only apply to P1.
- clamp to it's value range.
- use JudgeRes.
- refactor deduplicate judgeNotePress.

Fixes: db20d5c4873d ("feat: added autoadjust")

src/game/ruleset/ruleset_bms.cpp
@@ -1,5 +1,6 @@
 #include "ruleset_bms.h"
 
+#include <algorithm>
 #include <iterator>
 #include <utility>
 
@@ -1184,20 +1185,67 @@ void RulesetBMS::updateJudge(const lunaticvibes::Time& t, const NoteLaneIndex ch
     }
 }
 
-void RulesetBMS::updateAutoadjust(const HitableNote& pNote, const lunaticvibes::Time& rt)
+static bool isMainUserSide(RulesetBMS::PlaySide side)
 {
-    if (!State::get(IndexOption::PLAY_AUTOADJUST))
+    switch (side)
+    {
+    case RulesetBMS::PlaySide::SINGLE:
+    case RulesetBMS::PlaySide::DOUBLE:
+    case RulesetBMS::PlaySide::BATTLE_1P: return true;
+    case RulesetBMS::PlaySide::BATTLE_2P:
+    case RulesetBMS::PlaySide::AUTO:
+    case RulesetBMS::PlaySide::AUTO_2P:
+    case RulesetBMS::PlaySide::AUTO_DOUBLE:
+    case RulesetBMS::PlaySide::RIVAL:
+    case RulesetBMS::PlaySide::MYBEST:
+    case RulesetBMS::PlaySide::NETWORK: return false;
+    }
+    lunaticvibes::assert_failed("isMainUserSide");
+}
+
+static int clampVisualAdjust(int adjust)
+{
+    return std::clamp(adjust, -99, 99);
+}
+
+static bool isBadOrBetter(const RulesetBMS::JudgeArea area)
+{
+    switch (area)
+    {
+    case RulesetBMS::JudgeArea::EARLY_BAD:
+    case RulesetBMS::JudgeArea::EARLY_GOOD:
+    case RulesetBMS::JudgeArea::EARLY_GREAT:
+    case RulesetBMS::JudgeArea::EARLY_PERFECT:
+    case RulesetBMS::JudgeArea::EXACT_PERFECT:
+    case RulesetBMS::JudgeArea::LATE_PERFECT:
+    case RulesetBMS::JudgeArea::LATE_GREAT:
+    case RulesetBMS::JudgeArea::LATE_GOOD:
+    case RulesetBMS::JudgeArea::LATE_BAD: return true;
+    case RulesetBMS::JudgeArea::NOTHING:
+    case RulesetBMS::JudgeArea::EARLY_KPOOR:
+    case RulesetBMS::JudgeArea::MISS:
+    case RulesetBMS::JudgeArea::LATE_KPOOR:
+    case RulesetBMS::JudgeArea::MINE_KPOOR: return false;
+    }
+    lunaticvibes::assert_failed("isBadOrBetter");
+}
+
+void RulesetBMS::updateAutoadjust(const JudgeRes& j, const lunaticvibes::Time& rt)
+{
+    if (!isMainUserSide(_side))
         return;
-    bool isOffsetPositive = (rt - pNote.time).norm() >= 0;
-    lunaticvibes::Time hitOffset = rt > pNote.time ? rt - pNote.time : pNote.time - rt;
-    if (hitOffset <= judgeTime[(int)_judgeDifficulty].BAD)
+    if (State::get(IndexOption::PLAY_AUTOADJUST) == 0)
+        return;
+    const bool isOffsetPositive = j.time >= 0;
+    if (isBadOrBetter(j.area))
     {
         _notesSinceLastAutoadjust++;
         if (_notesSinceLastAutoadjust > 9)
         {
-            if (hitOffset.norm() != 0)
+            if (j.time.norm() != 0)
             {
-                int newAdjust = State::get(IndexNumber::TIMING_ADJUST_VISUAL) + (isOffsetPositive ? 1 : -1);
+                const int newAdjust =
+                    clampVisualAdjust(State::get(IndexNumber::TIMING_ADJUST_VISUAL) + (isOffsetPositive ? 1 : -1));
                 State::set(IndexNumber::TIMING_ADJUST_VISUAL, newAdjust);
             }
             _notesSinceLastAutoadjust = 0;
@@ -1205,42 +1253,41 @@ void RulesetBMS::updateAutoadjust(const HitableNote& pNote, const lunaticvibes::
     }
 }
 
+static std::pair<NoteLaneIndex, HitableNote*> getClosestNote(ChartObjectBase& chart, const Input::Pad k,
+                                                             const NoteLaneCategory cat)
+{
+    NoteLaneIndex idx = chart.getLaneFromKey(cat, k);
+    if (idx != _ && !chart.isLastNote(cat, idx))
+    {
+        auto itNote = chart.incomingNote(cat, idx);
+        while (!chart.isLastNote(cat, idx, itNote) && itNote->expired)
+            ++itNote;
+        if (!chart.isLastNote(cat, idx, itNote))
+            return {idx, &*itNote};
+    }
+    return {};
+}
+
 void RulesetBMS::judgeNotePress(const Input::Pad k, const lunaticvibes::Time& t, const lunaticvibes::Time& rt,
                                 const int slot)
 {
-    NoteLaneIndex idx1 = _chart->getLaneFromKey(NoteLaneCategory::Note, k);
-    HitableNote* pNote1 = nullptr;
-    if (idx1 != _ && !_chart->isLastNote(NoteLaneCategory::Note, idx1))
+    auto [idx, note] = getClosestNote(*_chart, k, NoteLaneCategory::Note);
+    auto category = NoteLaneCategory::Note;
     {
-        auto itNote = _chart->incomingNote(NoteLaneCategory::Note, idx1);
-        while (!_chart->isLastNote(NoteLaneCategory::Note, idx1, itNote) && itNote->expired)
-            ++itNote;
-        if (!_chart->isLastNote(NoteLaneCategory::Note, idx1, itNote))
-            pNote1 = &*itNote;
-    }
-    NoteLaneIndex idx2 = _chart->getLaneFromKey(NoteLaneCategory::LN, k);
-    HitableNote* pNote2 = nullptr;
-    if (idx2 != _ && !_chart->isLastNote(NoteLaneCategory::LN, idx2))
-    {
-        auto itNote = _chart->incomingNote(NoteLaneCategory::LN, idx2);
-        while (!_chart->isLastNote(NoteLaneCategory::LN, idx2, itNote) && itNote->expired)
-            ++itNote;
-        if (!_chart->isLastNote(NoteLaneCategory::LN, idx2, itNote))
-            pNote2 = &*itNote;
+        auto [idx2, note2] = getClosestNote(*_chart, k, NoteLaneCategory::LN);
+        if (note2 && (note == nullptr || note2->time < note->time))
+        {
+            idx = idx2;
+            note = note2;
+            category = NoteLaneCategory::LN;
+        }
     }
 
-    JudgeRes j;
-    if (pNote1 && (pNote2 == nullptr || pNote1->time < pNote2->time) && !pNote1->expired)
+    if (note && !note->expired)
     {
-        j = _calcJudgeByTimes(*pNote1, rt);
-        updateAutoadjust(*pNote1, rt);
-        _judgePress(NoteLaneCategory::Note, idx1, *pNote1, j, t, slot);
-    }
-    else if (pNote2 && !pNote2->expired)
-    {
-        j = _calcJudgeByTimes(*pNote2, rt);
-        updateAutoadjust(*pNote2, rt);
-        _judgePress(NoteLaneCategory::LN, idx2, *pNote2, j, t, slot);
+        const JudgeRes j = _calcJudgeByTimes(*note, rt);
+        updateAutoadjust(j, rt);
+        _judgePress(category, idx, *note, j, t, slot);
     }
 }
 void RulesetBMS::judgeNoteHold(Input::Pad k, const lunaticvibes::Time& t, const lunaticvibes::Time& rt, int slot)

src/game/ruleset/ruleset_bms.h
@@ -212,7 +212,7 @@ public:
 private:
     JudgeRes _calcJudgeByTimes(const Note& note, const lunaticvibes::Time& time) const;
 
-    void updateAutoadjust(const HitableNote& pNote, const lunaticvibes::Time& rt);
+    void updateAutoadjust(const JudgeRes& j, const lunaticvibes::Time& rt);
 
     void _judgePress(chart::NoteLaneCategory cat, chart::NoteLaneIndex idx, HitableNote& note, const JudgeRes& judge,
                      const lunaticvibes::Time& t, int slot);